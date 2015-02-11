594d8eecd30e24d91d1c3f8a61ab13e7267f9bcd
Erwin de Haan <git@erayan.eu>
2015-02-11 20:08:14 +0100

    Added isEmpty to History, fixed some best practices in the code.

--- src/main/java/com/rarchives/ripme/ripper/rippers/ChanRipper.java
+++ src/main/java/com/rarchives/ripme/ripper/rippers/ChanRipper.java
@@ -18,9 +18,12 @@ import com.rarchives.ripme.ripper.rippers.ripperhelpers.ChanSite;
 import com.rarchives.ripme.utils.Http;
 import com.rarchives.ripme.utils.RipUtils;
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import com.rarchives.ripme.utils.Utils;
 >>>>>>> Added same imgur and stuff album downloaders characteristics to ChanRipper.
+=======
+>>>>>>> Added isEmpty to History, fixed some best practices in the code.
 
 public class ChanRipper extends AbstractHTMLRipper {
     public static List<ChanSite> explicit_domains = Arrays.asList(
@@ -199,8 +202,9 @@ public class ChanRipper extends AbstractHTMLRipper {
                 }
 
                 List<URL> urls = RipUtils.getFilesFromURL(originalURL);
-                for (int i = 0; i < urls.size(); i++) {
-                    imageURLs.add(urls.get(i).toString());
+                //for (int i = 0; i < urls.size(); i++) {
+                for(URL imageurl : urls){
+                    imageURLs.add(imageurl.toString());
                 }
             }
 
