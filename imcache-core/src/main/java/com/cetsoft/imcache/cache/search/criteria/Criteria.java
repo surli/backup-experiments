bfb9e6832596be3af2a40bf7628e40f9d33149ad
Yusuf Aytas <yusufaytas@gmail.com>
2015-08-18 18:49:32 +0100

    Fixed formatting problems.

--- imcache-core/src/main/java/com/cetsoft/imcache/cache/search/criteria/Criteria.java
+++ imcache-core/src/main/java/com/cetsoft/imcache/cache/search/criteria/Criteria.java
@@ -1,21 +1,21 @@
 /*
-* Copyright (C) 2015 Cetsoft, http://www.cetsoft.com
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-* 
-* Author : Yusuf Aytas
-* Date   : Nov 8, 2013
-*/
+ * Copyright (C) 2015 Cetsoft, http://www.cetsoft.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author : Yusuf Aytas
+ * Date   : Nov 8, 2013
+ */
 package com.cetsoft.imcache.cache.search.criteria;
 
 /**
@@ -23,27 +23,27 @@ package com.cetsoft.imcache.cache.search.criteria;
  */
 public interface Criteria {
 
-	/**
-	 * And.
-	 *
-	 * @param criteria the criteria
-	 * @return the criteria
-	 */
-	Criteria and(Criteria criteria);
+    /**
+     * And.
+     *
+     * @param criteria the criteria
+     * @return the criteria
+     */
+    Criteria and(Criteria criteria);
 
-	/**
-	 * Or.
-	 *
-	 * @param criteria the criteria
-	 * @return the criteria
-	 */
-	Criteria or(Criteria criteria);
+    /**
+     * Or.
+     *
+     * @param criteria the criteria
+     * @return the criteria
+     */
+    Criteria or(Criteria criteria);
 
-	/**
-	 * Diff.
-	 *
-	 * @param criteria the criteria
-	 * @return the criteria
-	 */
-	Criteria diff(Criteria criteria);
+    /**
+     * Diff.
+     *
+     * @param criteria the criteria
+     * @return the criteria
+     */
+    Criteria diff(Criteria criteria);
 }
