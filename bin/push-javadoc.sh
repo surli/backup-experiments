b4ae79dfe3ad1460d477905db50f998a3cb3393c
Jonathan Halterman <jhalterman@gmail.com>
2016-08-24 10:01:40 -0700

    Reset expiration time during sorted entry reordering. Fixes issue #36.

---
+++ bin/push-javadoc.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# run from top level dir
+
+PROJECT=expiringmap
+
+mvn javadoc:javadoc -Djv=$apiVersion
+rm -rf target/docs
+git clone git@github.com:jhalterman/$PROJECT.git target/docs -b gh-pages
+cd target/docs
+git rm -rf javadoc
+mkdir -p javadoc
+mv -v ../site/apidocs/* javadoc
+git add -A -f javadoc
+git commit -m "Updated JavaDocs"
+git push -fq origin gh-pages > /dev/null
+
+echo "Published JavaDocs"
