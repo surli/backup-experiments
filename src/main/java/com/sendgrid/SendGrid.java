24f9f3bf30a09a054aea96e3d9b40c1caa6540f2
Marcos Barbero <marcos.hgb@gmail.com>
2016-10-07 11:58:45 -0300

    Allow a timeout value to be set

--- src/main/java/com/sendgrid/SendGrid.java
+++ src/main/java/com/sendgrid/SendGrid.java
@@ -1,113 +1,130 @@
 package com.sendgrid;
 
-import com.sendgrid.Client;
-import com.sendgrid.Method;
-import com.sendgrid.Request;
-import com.sendgrid.Response;
+import org.apache.http.impl.client.CloseableHttpClient;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
-  * Class SendGrid allows for quick and easy access to the SendGrid API.
-  */
+ * Class SendGrid allows for quick and easy access to the SendGrid API.
+ */
 public class SendGrid {
-  private static final String VERSION = "3.0.0";
-  private static final String USER_AGENT = "sendgrid/" + VERSION + ";java";
+    private static final String VERSION = "3.0.0";
+    private static final String USER_AGENT = "sendgrid/" + VERSION + ";java";
 
-  private String apiKey;
-  private String host;
-  private String version;
-  private Client client;
-  private Map<String,String> requestHeaders;
+    private String apiKey;
+    private String host;
+    private String version;
+    private Client client;
+    private Map<String, String> requestHeaders;
 
-  /**
-    * @param apiKey is your SendGrid API Key: https://app.sendgrid.com/settings/api_keys
-    */
-  public SendGrid(String apiKey) {
-    this.client = new Client();
-    initializeSendGrid(apiKey);
-  }
-
-  /**
-    * @param apiKey is your SendGrid API Key: https://app.sendgrid.com/settings/api_keys
-    * @param test is true if you are unit testing
-    */
-  public SendGrid(String apiKey, Boolean test) {
-    this.client = new Client(test);
-    initializeSendGrid(apiKey);
-  }
-
-  public void initializeSendGrid(String apiKey) {
-    this.apiKey = apiKey;
-    this.host = "api.sendgrid.com";
-    this.version = "v3";
-    this.requestHeaders = new HashMap<String, String>();
-    this.requestHeaders.put("Authorization", "Bearer " + apiKey);
-    this.requestHeaders.put("User-agent", USER_AGENT);
-    this.requestHeaders.put("Accept", "application/json");
-  }
-
-  public String getLibraryVersion() {
-    return this.VERSION;
-  }
-
-  public String getVersion() {
-    return this.version;
-  }
-
-  public void setVersion(String version) {
-    this.version = version;
-  }
-
-  public Map<String,String> getRequestHeaders() {
-    return this.requestHeaders;
-  }
-
-  public Map<String,String> addRequestHeader(String key, String value) {
-    this.requestHeaders.put(key, value);
-    return getRequestHeaders();
-  }
-
-  public Map<String,String> removeRequestHeader(String key) {
-    this.requestHeaders.remove(key);
-    return getRequestHeaders();
-  }
-
-  public String getHost() {
-    return this.host;
-  }
-
-  public void setHost(String host) {
-    this.host = host;
-  }
-
-  /**
-    * Class makeCall makes the call to the SendGrid API, override this method for testing.
-    */
-  public Response makeCall(Request request) throws IOException {
-    Response response = new Response();
-    try {
-      response = client.api(request);
-    } catch (IOException ex) {
-      throw ex;
+    /**
+     * @param apiKey is your SendGrid API Key: https://app.sendgrid.com/settings/api_keys
+     */
+    public SendGrid(String apiKey) {
+        this.client = new Client();
+        initializeSendGrid(apiKey);
     }
-    return response;
-  }
 
-  /**
-    * Class api sets up the request to the SendGrid API, this is main interface.
-    */
-  public Response api(Request request) throws IOException {
-    Request req = new Request();
-    req.method = request.method;
-    req.baseUri = this.host;
-    req.endpoint = "/" + version + "/" + request.endpoint;
-    req.body = request.body;
-    req.headers = this.requestHeaders;
-    req.queryParams = request.queryParams;
+    /**
+     * @param apiKey is your SendGrid API Key: https://app.sendgrid.com/settings/api_keys
+     * @param test   is true if you are unit testing
+     */
+    public SendGrid(String apiKey, Boolean test) {
+        this.client = new Client(test);
+        initializeSendGrid(apiKey);
+    }
 
-    return makeCall(req);
-  }
+    /**
+     * @param apiKey              is your SendGrid API Key: https://app.sendgrid.com/settings/api_keys
+     * @param closeableHttpClient pass your own tuned client to be used
+     *                            <p>Example</p>
+     *                            <pre>
+     *                                 PoolingHttpClientConnectionManager poolingHttpClientConnectionManager = new PoolingHttpClientConnectionManager();
+     *                                 poolingHttpClientConnectionManager.setMaxTotal(10_000);
+     *                                 poolingHttpClientConnectionManager.setDefaultMaxPerRoute(10_000);
+     *                                 RequestConfig defaultRequestConfig = RequestConfig.custom()
+     *                                 .setSocketTimeout(5000)
+     *                                 .setConnectTimeout(5000)
+     *                                 .setConnectionRequestTimeout(5000)
+     *                                 .build();
+     *                                 CloseableHttpClient httpClient = HttpClients.custom()
+     *                                 .setDefaultRequestConfig(defaultRequestConfig)
+     *                                 .setConnectionManager(poolingHttpClientConnectionManager)
+     *                                 .build();
+     *
+     *                                    new SendGrid("xxxx", httpclient);
+     *                            </pre>
+     */
+    public SendGrid(String apiKey, CloseableHttpClient closeableHttpClient) {
+        this.client = new Client(closeableHttpClient);
+        initializeSendGrid(apiKey);
+    }
+
+    public void initializeSendGrid(String apiKey) {
+        this.apiKey = apiKey;
+        this.host = "api.sendgrid.com";
+        this.version = "v3";
+        this.requestHeaders = new HashMap<String, String>();
+        this.requestHeaders.put("Authorization", "Bearer " + apiKey);
+        this.requestHeaders.put("User-agent", USER_AGENT);
+        this.requestHeaders.put("Accept", "application/json");
+    }
+
+    public String getLibraryVersion() {
+        return VERSION;
+    }
+
+    public String getVersion() {
+        return this.version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public Map<String, String> getRequestHeaders() {
+        return this.requestHeaders;
+    }
+
+    public Map<String, String> addRequestHeader(String key, String value) {
+        this.requestHeaders.put(key, value);
+        return getRequestHeaders();
+    }
+
+    public Map<String, String> removeRequestHeader(String key) {
+        this.requestHeaders.remove(key);
+        return getRequestHeaders();
+    }
+
+    public String getHost() {
+        return this.host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    /**
+     * Class makeCall makes the call to the SendGrid API, override this method for testing.
+     */
+    public Response makeCall(Request request) throws IOException {
+        return client.api(request);
+    }
+
+    /**
+     * Class api sets up the request to the SendGrid API, this is main interface.
+     */
+    public Response api(Request request) throws IOException {
+        Request req = new Request();
+        req.method = request.method;
+        req.baseUri = this.host;
+        req.endpoint = "/" + version + "/" + request.endpoint;
+        req.body = request.body;
+        req.headers = this.requestHeaders;
+        req.queryParams = request.queryParams;
+
+        return makeCall(req);
+    }
 }
