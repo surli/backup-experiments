89219dd52f32de71b86361f3711cc84a3e195138
Lukasz Osipiuk <lukasz@osipiuk.net>
2016-11-17 13:10:30 +0100

    Intoduce stream-spiller based GenericSpiller
    Add concept of SingleStreamSpiller which is responsible of
    spilling single stream of data.
    A set of single stream spiller is wrapped with in GenericSpiller class.
    Old BinaryFileSpiller is replaced with GenericSpill parametrized with
    BinaryFileSingleStreamSpiller factory.

---
+++ presto-main/src/main/java/com/facebook/presto/spiller/FileSingleStreamSpiller.java
@@ -0,0 +1,144 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.spiller;
+
+import com.facebook.presto.execution.buffer.PagesSerde;
+import com.facebook.presto.execution.buffer.PagesSerdeUtil;
+import com.facebook.presto.execution.buffer.SerializedPage;
+import com.facebook.presto.operator.SpillContext;
+import com.facebook.presto.spi.Page;
+import com.facebook.presto.spi.PrestoException;
+import com.google.common.io.Closer;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import io.airlift.slice.InputStreamSliceInput;
+import io.airlift.slice.OutputStreamSliceOutput;
+import io.airlift.slice.SliceOutput;
+
+import javax.annotation.concurrent.NotThreadSafe;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Iterator;
+
+import static com.facebook.presto.execution.buffer.PagesSerdeUtil.writeSerializedPage;
+import static com.facebook.presto.spi.StandardErrorCode.GENERIC_INTERNAL_ERROR;
+import static com.google.common.base.Preconditions.checkState;
+import static java.util.Objects.requireNonNull;
+
+@NotThreadSafe
+public class FileSingleStreamSpiller
+        implements SingleStreamSpiller
+{
+    private final Path targetFileName;
+    private final Closer closer = Closer.create();
+    private final PagesSerde serde;
+    private final SpillerStats spillerStats;
+    private final SpillContext localSpillContext;
+
+    private final ListeningExecutorService executor;
+
+    private ListenableFuture<?> spillInProgress = Futures.immediateFuture(null);
+
+    public FileSingleStreamSpiller(
+            PagesSerde serde,
+            ListeningExecutorService executor,
+            Path spillPath,
+            SpillerStats spillerStats,
+            SpillContext spillContext)
+    {
+        this.serde = requireNonNull(serde, "serde is null");
+        this.executor = requireNonNull(executor, "executor is null");
+        this.spillerStats = requireNonNull(spillerStats, "spillerStats is null");
+        this.localSpillContext = spillContext.newLocalSpillContext();
+        try {
+            targetFileName = Files.createTempFile(spillPath, "spill", ".bin");
+        }
+        catch (IOException e) {
+            throw new PrestoException(GENERIC_INTERNAL_ERROR, "Failed to create spill file", e);
+        }
+    }
+
+    @Override
+    public ListenableFuture<?> spill(Iterator<Page> pageIterator)
+    {
+        checkNoSpillInProgress();
+        spillInProgress = executor.submit(() -> writePages(pageIterator));
+        return spillInProgress;
+    }
+
+    @Override
+    public Iterator<Page> getSpilledPages()
+    {
+        checkNoSpillInProgress();
+        return readPages();
+    }
+
+    private void writePages(Iterator<Page> pageIterator)
+    {
+        try (SliceOutput output = new OutputStreamSliceOutput(new FileOutputStream(targetFileName.toFile(), true))) {
+            while (pageIterator.hasNext()) {
+                Page page = pageIterator.next();
+                SerializedPage serializedPage = serde.serialize(page);
+                long pageSize = serializedPage.getSizeInBytes();
+                localSpillContext.updateBytes(pageSize);
+                spillerStats.addToTotalSpilledBytes(pageSize);
+                writeSerializedPage(output, serializedPage);
+            }
+        }
+        catch (UncheckedIOException | IOException e) {
+            throw new PrestoException(GENERIC_INTERNAL_ERROR, "Failed to spill pages", e);
+        }
+    }
+
+    private Iterator<Page> readPages()
+    {
+        try {
+            InputStream input = new FileInputStream(targetFileName.toFile());
+            closer.register(input);
+            return PagesSerdeUtil.readPages(serde, new InputStreamSliceInput(input));
+        }
+        catch (IOException e) {
+            throw new PrestoException(GENERIC_INTERNAL_ERROR, "Failed to read spilled pages", e);
+        }
+    }
+
+    @Override
+    public void close()
+    {
+        closer.register(() -> Files.delete(targetFileName));
+        closer.register(localSpillContext);
+
+        try {
+            closer.close();
+        }
+        catch (Exception e) {
+            throw new PrestoException(
+                    GENERIC_INTERNAL_ERROR,
+                    "Failed to close spiller",
+                    e);
+        }
+    }
+
+    private void checkNoSpillInProgress()
+    {
+        checkState(spillInProgress.isDone(), "spill in progress");
+    }
+}
