dbc151b4b743ea2bbf9cd40b46a272c5934d54a7
AlexElin <alexey.elin@list.ru>
2017-01-22 19:19:51 +0600

    refactor: remove checks for jdk version 1.4 (tests) (#737)
    remove checks for jdk version 1.4 in the tests

---
+++ pgjdbc/src/main/java/org/postgresql/jdbc2/optional/SimpleDataSource.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2004, PostgreSQL Global Development Group
+ * See the LICENSE file in the project root for more information.
+ */
+
+package org.postgresql.jdbc2.optional;
+
+import org.postgresql.ds.PGSimpleDataSource;
+
+public class SimpleDataSource extends PGSimpleDataSource {
+}
---
+++ pgjdbc/src/main/java/org/postgresql/jdbc3/Jdbc3ConnectionPool.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2004, PostgreSQL Global Development Group
+ * See the LICENSE file in the project root for more information.
+ */
+
+package org.postgresql.jdbc3;
+
+import org.postgresql.ds.PGConnectionPoolDataSource;
+
+public class Jdbc3ConnectionPool extends PGConnectionPoolDataSource {
+}
