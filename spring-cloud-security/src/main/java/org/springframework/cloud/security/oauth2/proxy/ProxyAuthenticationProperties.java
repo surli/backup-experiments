fe4a640d592f6c59bd5350bb99f902804925dc3d
Spencer Gibb <spencer@gibb.us>
2017-01-27 11:10:44 -0500

    Merge pull request #106 from Infeligo/master
    Allow setting custom AccessTokenProvider on OAuth2FeignRequestInterceptor

---
+++ spring-cloud-security/src/main/java/org/springframework/cloud/security/oauth2/proxy/ProxyAuthenticationProperties.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2013-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.cloud.security.oauth2.proxy;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.annotation.PostConstruct;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author Dave Syer
+ *
+ */
+@ConfigurationProperties("proxy.auth")
+public class ProxyAuthenticationProperties {
+
+	/**
+	 * Authentication strategy per route. 
+	 */
+	@Getter
+	@Setter
+	private Map<String, Route> routes = new HashMap<String, Route>();
+	
+	@Getter
+	@Setter
+	private boolean loadBalanced;
+
+	@PostConstruct
+	public void init() {
+		for (Entry<String, Route> entry : routes.entrySet()) {
+			if (entry.getValue().getId() == null) {
+				entry.getValue().setId(entry.getKey());
+			}
+		}
+	}
+
+	@Data
+	public static class Route {
+		/**
+		 * The id of the route (e.g. discovery virtual hostname).
+		 */
+		private String id;
+		/**
+		 * The authentication scheme to use (e.g. "oauth2", "none").
+		 */
+		private String scheme;
+
+		public Route(String scheme) {
+			this.scheme = scheme;
+		}
+
+		public static class Scheme {
+
+			public static final Scheme OAUTH2 = new Scheme("oauth2");
+			public static final Scheme PASSTHRU = new Scheme("passthru");
+			public static final Scheme NONE = new Scheme("none");
+			private final String value;
+
+			private Scheme(String value) {
+				this.value = value;
+			}
+
+			public boolean matches(String value) {
+				return value!=null && value.equals(this.value);
+			}
+		}
+	}
+
+}
---
+++ spring-cloud-security/src/test/java/org/springframework/cloud/security/oauth2/client/OAuth2LoadBalancerClientAutoConfigurationTests.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.security.oauth2.client;
+
+import static org.junit.Assert.assertFalse;
+
+import java.net.URI;
+
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.client.ClientHttpRequest;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+
+/**
+ * @author Dave Syer
+ *
+ */
+public class OAuth2LoadBalancerClientAutoConfigurationTests {
+
+	private ConfigurableApplicationContext context;
+
+	@Rule
+	public ExpectedException expected = ExpectedException.none();
+
+	@After
+	public void close() {
+		if (this.context != null) {
+			this.context.close();
+		}
+	}
+
+	@Test
+	public void clientNotConfigured() {
+		this.context = new SpringApplicationBuilder(NoClientConfiguration.class)
+				.properties("spring.config.name=test", "server.port=0",
+						"security.oauth2.resource.userInfoUri:http://example.com")
+				.run();
+		assertFalse(this.context.containsBean("loadBalancedOauth2RestTemplate"));
+	}
+
+	@Test
+	public void clientConfigured() throws Exception {
+		this.context = new SpringApplicationBuilder(ClientConfiguration.class)
+				.properties("spring.config.name=test", "server.port=0",
+						"security.oauth2.resource.userInfoUri:http://example.com",
+						"security.oauth2.client.clientId=foo")
+				.run();
+		OAuth2RestTemplate template = this.context
+				.getBean("loadBalancedOauth2RestTemplate", OAuth2RestTemplate.class);
+		ClientHttpRequest request = template.getRequestFactory()
+				.createRequest(new URI("http://nosuchservice"), HttpMethod.GET);
+		expected.expectMessage("No instances available for nosuchservice");
+		request.execute();
+	}
+
+	@EnableAutoConfiguration
+	@Configuration
+	protected static class NoClientConfiguration {
+	}
+
+	@EnableAutoConfiguration
+	@Configuration
+	@EnableOAuth2Sso
+	protected static class ClientConfiguration {
+
+		@LoadBalanced
+		@Bean
+		public OAuth2RestTemplate loadBalancedOauth2RestTemplate(
+				OAuth2ProtectedResourceDetails resource,
+				OAuth2ClientContext oauth2Context) {
+			return new OAuth2RestTemplate(resource, oauth2Context);
+		}
+
+	}
+}
