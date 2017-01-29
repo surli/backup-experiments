efe7f31494d5d19c5d6588dc7e426bed0f3a96e6
megaboich <mega.boich@gmail.com>
2017-01-29 16:22:48 +0100

    Fix Typescript errors with 'strictNullChecks' mode enabled (#4658)
    error TS2454: Variable 'contentTypeHeader' is used before being assigned.
    error TS2345: Argument of type 'FetchAPI | undefined' is not assignable to parameter of type 'FetchAPI'.

---
+++ samples/server/petstore/jaxrs-cxf-cdi/src/gen/java/io/swagger/model/Order.java
@@ -0,0 +1,194 @@
+package io.swagger.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+
+
+/**
+ * An order for a pets from the pet store
+ **/
+
+import io.swagger.annotations.*;
+import java.util.Objects;
+@ApiModel(description = "An order for a pets from the pet store")
+
+public class Order   {
+  
+  private Long id = null;
+  private Long petId = null;
+  private Integer quantity = null;
+  private java.util.Date shipDate = null;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType(name="Order")
+@XmlEnum
+public enum Order {
+    {values&#x3D;[placed, approved, delivered], enumVars&#x3D;[{name&#x3D;PLACED, value&#x3D;&quot;placed&quot;}, {name&#x3D;APPROVED, value&#x3D;&quot;approved&quot;}, {name&#x3D;DELIVERED, value&#x3D;&quot;delivered&quot;}]}, 
+    
+    public String value() {
+        return name();
+    }
+
+    public static Order fromValue(String v) {
+        return valueOf(v);
+    }
+}
+  private StatusEnum status = null;
+  private Boolean complete = false;
+
+  /**
+   **/
+  public Order id(Long id) {
+    this.id = id;
+    return this;
+  }
+
+  
+  @ApiModelProperty(example = "null", value = "")
+  @JsonProperty("id")
+  public Long getId() {
+    return id;
+  }
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  /**
+   **/
+  public Order petId(Long petId) {
+    this.petId = petId;
+    return this;
+  }
+
+  
+  @ApiModelProperty(example = "null", value = "")
+  @JsonProperty("petId")
+  public Long getPetId() {
+    return petId;
+  }
+  public void setPetId(Long petId) {
+    this.petId = petId;
+  }
+
+  /**
+   **/
+  public Order quantity(Integer quantity) {
+    this.quantity = quantity;
+    return this;
+  }
+
+  
+  @ApiModelProperty(example = "null", value = "")
+  @JsonProperty("quantity")
+  public Integer getQuantity() {
+    return quantity;
+  }
+  public void setQuantity(Integer quantity) {
+    this.quantity = quantity;
+  }
+
+  /**
+   **/
+  public Order shipDate(java.util.Date shipDate) {
+    this.shipDate = shipDate;
+    return this;
+  }
+
+  
+  @ApiModelProperty(example = "null", value = "")
+  @JsonProperty("shipDate")
+  public java.util.Date getShipDate() {
+    return shipDate;
+  }
+  public void setShipDate(java.util.Date shipDate) {
+    this.shipDate = shipDate;
+  }
+
+  /**
+   * Order Status
+   **/
+  public Order status(StatusEnum status) {
+    this.status = status;
+    return this;
+  }
+
+  
+  @ApiModelProperty(example = "null", value = "Order Status")
+  @JsonProperty("status")
+  public StatusEnum getStatus() {
+    return status;
+  }
+  public void setStatus(StatusEnum status) {
+    this.status = status;
+  }
+
+  /**
+   **/
+  public Order complete(Boolean complete) {
+    this.complete = complete;
+    return this;
+  }
+
+  
+  @ApiModelProperty(example = "null", value = "")
+  @JsonProperty("complete")
+  public Boolean getComplete() {
+    return complete;
+  }
+  public void setComplete(Boolean complete) {
+    this.complete = complete;
+  }
+
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Order order = (Order) o;
+    return Objects.equals(id, order.id) &&
+        Objects.equals(petId, order.petId) &&
+        Objects.equals(quantity, order.quantity) &&
+        Objects.equals(shipDate, order.shipDate) &&
+        Objects.equals(status, order.status) &&
+        Objects.equals(complete, order.complete);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, petId, quantity, shipDate, status, complete);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Order {\n");
+    
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    petId: ").append(toIndentedString(petId)).append("\n");
+    sb.append("    quantity: ").append(toIndentedString(quantity)).append("\n");
+    sb.append("    shipDate: ").append(toIndentedString(shipDate)).append("\n");
+    sb.append("    status: ").append(toIndentedString(status)).append("\n");
+    sb.append("    complete: ").append(toIndentedString(complete)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
