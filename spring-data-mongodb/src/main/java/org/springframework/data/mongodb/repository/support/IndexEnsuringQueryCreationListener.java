spring-projects/spring-data-mongodb

DATAMONGO-1602 - Remove references to Assert single-arg methods.
Replace references to Assert single-arg methods with references to methods accepting the test object and message.

Related ticket: SPR-15196.

spring-data-mongodb/src/main/java/org/springframework/data/mongodb/repository/support/IndexEnsuringQueryCreationListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2013 the original author or authors.
+ * Copyright 2011-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -57,7 +57,7 @@ class IndexEnsuringQueryCreationListener implements QueryCreationListener<PartTr
 	 */
 	public IndexEnsuringQueryCreationListener(IndexOperationsProvider indexOperationsProvider) {
 
-		Assert.notNull(indexOperationsProvider);
+		Assert.notNull(indexOperationsProvider, "IndexOperationsProvider must not be null!");
 		this.indexOperationsProvider = indexOperationsProvider;
 	}
 

surli/backup-experiments

spring-data-mongodb/src/main/java/org/springframework/data/mongodb/repository/support/IndexEnsuringQueryCreationListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2013 the original author or authors.
+ * Copyright 2011-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -53,7 +53,7 @@ class IndexEnsuringQueryCreationListener implements QueryCreationListener<PartTr
 	 */
 	public IndexEnsuringQueryCreationListener(MongoOperations operations) {
 
-		Assert.notNull(operations);
+		Assert.notNull(operations, "MongoOperations must not be null!");
 		this.operations = operations;
 	}
 