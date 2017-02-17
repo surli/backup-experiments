de30c8d2a8859f1ef4bd620389ab077078f2a5cb
mhajas <mhajas@redhat.com>
2017-02-17 08:48:50 +0100

    KEYCLOAK-3955

--- testsuite/integration-arquillian/test-apps/cors/angular-product/src/main/webapp/lib/angular/jstd-scenario-adapter-config.js
+++ testsuite/integration-arquillian/test-apps/cors/angular-product/src/main/webapp/lib/angular/jstd-scenario-adapter-config.js
@@ -15,29 +15,9 @@
  * limitations under the License.
  */
 
-package org.keycloak.testsuite.adapter.page;
-
-import org.jboss.arquillian.container.test.api.OperateOnDeployment;
-import org.jboss.arquillian.test.api.ArquillianResource;
-import org.keycloak.testsuite.page.AbstractPageWithInjectedUrl;
-
-import java.net.URL;
-
 /**
- *
- * @author tkyjovsk
+ * Configuration for jstd scenario adapter
  */
-public class CorsDatabaseServiceExample extends AbstractPageWithInjectedUrl {
-
-    public static final String DEPLOYMENT_NAME = "cors-database-service";
-
-    @ArquillianResource
-    @OperateOnDeployment(DEPLOYMENT_NAME)
-    private URL url;
-
-    @Override
-    public URL getInjectedUrl() {
-        return url;
-    }
-
-}
+var jstdScenarioAdapter = {
+  relativeUrlPrefix: '/build/docs/'
+};
