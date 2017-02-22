5bae4bfe8ce3862c7b97e62648eafdb8103408bc
Gian Merlino <gianmerlino@gmail.com>
2017-02-21 16:03:31 -0800

    Ability to filter on virtual columns. (#3942)
    This didn't need much other than having BitmapIndexSelector return null from
    various methods to trigger cursor based filtering.

---
+++ processing/src/main/java/io/druid/query/timeseries/TimeseriesQuery.java
@@ -0,0 +1,231 @@
+/*
+ * Licensed to Metamarkets Group Inc. (Metamarkets) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Metamarkets licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.druid.query.timeseries;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.google.common.collect.ImmutableList;
+import io.druid.granularity.QueryGranularity;
+import io.druid.query.BaseQuery;
+import io.druid.query.DataSource;
+import io.druid.query.Queries;
+import io.druid.query.Query;
+import io.druid.query.Result;
+import io.druid.query.aggregation.AggregatorFactory;
+import io.druid.query.aggregation.PostAggregator;
+import io.druid.query.filter.DimFilter;
+import io.druid.query.spec.QuerySegmentSpec;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ */
+@JsonTypeName("timeseries")
+public class TimeseriesQuery extends BaseQuery<Result<TimeseriesResultValue>>
+{
+  private final DimFilter dimFilter;
+  private final QueryGranularity granularity;
+  private final List<AggregatorFactory> aggregatorSpecs;
+  private final List<PostAggregator> postAggregatorSpecs;
+
+  @JsonCreator
+  public TimeseriesQuery(
+      @JsonProperty("dataSource") DataSource dataSource,
+      @JsonProperty("intervals") QuerySegmentSpec querySegmentSpec,
+      @JsonProperty("descending") boolean descending,
+      @JsonProperty("filter") DimFilter dimFilter,
+      @JsonProperty("granularity") QueryGranularity granularity,
+      @JsonProperty("aggregations") List<AggregatorFactory> aggregatorSpecs,
+      @JsonProperty("postAggregations") List<PostAggregator> postAggregatorSpecs,
+      @JsonProperty("context") Map<String, Object> context
+  )
+  {
+    super(dataSource, querySegmentSpec, descending, context);
+    this.dimFilter = dimFilter;
+    this.granularity = granularity;
+    this.aggregatorSpecs = aggregatorSpecs == null ? ImmutableList.<AggregatorFactory>of() : aggregatorSpecs;
+    this.postAggregatorSpecs = postAggregatorSpecs == null ? ImmutableList.<PostAggregator>of() : postAggregatorSpecs;
+
+    Queries.verifyAggregations(this.aggregatorSpecs, this.postAggregatorSpecs);
+  }
+
+  @Override
+  public boolean hasFilters()
+  {
+    return dimFilter != null;
+  }
+
+  @Override
+  public DimFilter getFilter()
+  {
+    return dimFilter;
+  }
+
+  @Override
+  public String getType()
+  {
+    return Query.TIMESERIES;
+  }
+
+  @JsonProperty("filter")
+  public DimFilter getDimensionsFilter()
+  {
+    return dimFilter;
+  }
+
+  @JsonProperty
+  public QueryGranularity getGranularity()
+  {
+    return granularity;
+  }
+
+  @JsonProperty("aggregations")
+  public List<AggregatorFactory> getAggregatorSpecs()
+  {
+    return aggregatorSpecs;
+  }
+
+  @JsonProperty("postAggregations")
+  public List<PostAggregator> getPostAggregatorSpecs()
+  {
+    return postAggregatorSpecs;
+  }
+
+  public boolean isSkipEmptyBuckets()
+  {
+    return getContextBoolean("skipEmptyBuckets", false);
+  }
+
+  public TimeseriesQuery withQuerySegmentSpec(QuerySegmentSpec querySegmentSpec)
+  {
+    return new TimeseriesQuery(
+        getDataSource(),
+        querySegmentSpec,
+        isDescending(),
+        dimFilter,
+        granularity,
+        aggregatorSpecs,
+        postAggregatorSpecs,
+        getContext()
+    );
+  }
+
+  @Override
+  public Query<Result<TimeseriesResultValue>> withDataSource(DataSource dataSource)
+  {
+    return new TimeseriesQuery(
+        dataSource,
+        getQuerySegmentSpec(),
+        isDescending(),
+        dimFilter,
+        granularity,
+        aggregatorSpecs,
+        postAggregatorSpecs,
+        getContext()
+    );
+  }
+
+  public TimeseriesQuery withOverriddenContext(Map<String, Object> contextOverrides)
+  {
+    return new TimeseriesQuery(
+        getDataSource(),
+        getQuerySegmentSpec(),
+        isDescending(),
+        dimFilter,
+        granularity,
+        aggregatorSpecs,
+        postAggregatorSpecs,
+        computeOverridenContext(contextOverrides)
+    );
+  }
+
+  public TimeseriesQuery withDimFilter(DimFilter dimFilter)
+  {
+    return new TimeseriesQuery(
+        getDataSource(),
+        getQuerySegmentSpec(),
+        isDescending(),
+        dimFilter,
+        granularity,
+        aggregatorSpecs,
+        postAggregatorSpecs,
+        getContext()
+    );
+  }
+
+  @Override
+  public String toString()
+  {
+    return "TimeseriesQuery{" +
+        "dataSource='" + getDataSource() + '\'' +
+        ", querySegmentSpec=" + getQuerySegmentSpec() +
+        ", descending=" + isDescending() +
+        ", dimFilter=" + dimFilter +
+        ", granularity='" + granularity + '\'' +
+        ", aggregatorSpecs=" + aggregatorSpecs +
+        ", postAggregatorSpecs=" + postAggregatorSpecs +
+        ", context=" + getContext() +
+        '}';
+  }
+
+  @Override
+  public boolean equals(Object o)
+  {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    if (!super.equals(o)) {
+      return false;
+    }
+
+    TimeseriesQuery that = (TimeseriesQuery) o;
+
+    if (aggregatorSpecs != null ? !aggregatorSpecs.equals(that.aggregatorSpecs) : that.aggregatorSpecs != null) {
+      return false;
+    }
+    if (dimFilter != null ? !dimFilter.equals(that.dimFilter) : that.dimFilter != null) {
+      return false;
+    }
+    if (granularity != null ? !granularity.equals(that.granularity) : that.granularity != null) {
+      return false;
+    }
+    if (postAggregatorSpecs != null ? !postAggregatorSpecs.equals(that.postAggregatorSpecs) : that.postAggregatorSpecs != null) {
+      return false;
+    }
+
+    return true;
+  }
+
+  @Override
+  public int hashCode()
+  {
+    int result = super.hashCode();
+    result = 31 * result + (dimFilter != null ? dimFilter.hashCode() : 0);
+    result = 31 * result + (granularity != null ? granularity.hashCode() : 0);
+    result = 31 * result + (aggregatorSpecs != null ? aggregatorSpecs.hashCode() : 0);
+    result = 31 * result + (postAggregatorSpecs != null ? postAggregatorSpecs.hashCode() : 0);
+    return result;
+  }
+}
