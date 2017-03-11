2fe49ec7be5923fa582739f717462dde7c4cea31
Wenlei Xie <wxie@fb.com>
2017-03-11 13:10:51 -0800

    Fix and rewrite casting complex type to JSON
    The current implementation of casting complex type to JSON is based on
    Type.getObjectValue(), which should not be used for CAST but only for
    REST service.

--- presto-main/src/test/java/com/facebook/presto/type/TestJsonOperators.java
+++ presto-main/src/test/java/com/facebook/presto/type/TestJsonOperators.java
@@ -197,4 +197,183 @@ public class TestJsonOperators
         assertFunction("cast('abc' as JSON)", JSON, "\"abc\"");
         assertFunction("cast('\"a\":2' as JSON)", JSON, "\"\\\"a\\\":2\"");
     }
+
+    @Test
+    public void testCastFromArray()
+    {
+        assertFunction("cast(cast (null as ARRAY<BIGINT>) AS JSON)", JSON, null);
+        assertFunction("cast(ARRAY[] AS JSON)", JSON, "[]");
+        assertFunction("cast(ARRAY[null, null] AS JSON)", JSON, "[null,null]");
+
+        assertFunction("cast(ARRAY[true, false, null] AS JSON)", JSON, "[true,false,null]");
+
+        assertFunction("cast(cast(ARRAY[1, 2, null] AS ARRAY<TINYINT>) AS JSON)", JSON, "[1,2,null]");
+        assertFunction("cast(cast(ARRAY[12345, -12345, null] AS ARRAY<SMALLINT>) AS JSON)", JSON, "[12345,-12345,null]");
+        assertFunction("cast(cast(ARRAY[123456789, -123456789, null] AS ARRAY<INTEGER>) AS JSON)", JSON, "[123456789,-123456789,null]");
+        assertFunction("cast(cast(ARRAY[1234567890123456789, -1234567890123456789, null] AS ARRAY<BIGINT>) AS JSON)", JSON, "[1234567890123456789,-1234567890123456789,null]");
+
+        assertFunction("CAST(CAST(ARRAY[3.14, nan(), infinity(), -infinity(), null] AS ARRAY<REAL>) AS JSON)", JSON, "[3.14,\"NaN\",\"Infinity\",\"-Infinity\",null]");
+        assertFunction("CAST(ARRAY[3.14, 1e-323, 1e308, nan(), infinity(), -infinity(), null] AS JSON)", JSON, "[3.14,1.0E-323,1.0E308,\"NaN\",\"Infinity\",\"-Infinity\",null]");
+        assertFunction("CAST(ARRAY[DECIMAL '3.14', null] AS JSON)", JSON, "[3.14,null]");
+        assertFunction("CAST(ARRAY[DECIMAL '12345678901234567890.123456789012345678', null] AS JSON)", JSON, "[12345678901234567890.123456789012345678,null]");
+
+        assertFunction("cast(ARRAY['a', 'bb', null] AS JSON)", JSON, "[\"a\",\"bb\",null]");
+        assertFunction(
+                "cast(ARRAY[JSON '123', JSON '3.14', JSON 'false', JSON '\"abc\"', JSON '[1, \"a\", null]', JSON '{\"a\": 1, \"b\": \"str\", \"c\": null}', JSON 'null', null] AS JSON)",
+                JSON,
+                "[123,3.14,false,\"abc\",[1,\"a\",null],{\"a\":1,\"b\":\"str\",\"c\":null},null,null]");
+
+        assertFunction(
+                "cast(ARRAY[ARRAY[1, 2], ARRAY[3, null], ARRAY[], ARRAY[null, null], null] AS JSON)",
+                JSON,
+                "[[1,2],[3,null],[],[null,null],null]");
+        assertFunction(
+                "cast(ARRAY[MAP(ARRAY['a', 'b'], ARRAY[1, 2]), MAP(ARRAY['three', 'none'], ARRAY[3, null]), MAP(), MAP(ARRAY['h1', 'h2'], ARRAY[null, null]), null] AS JSON)",
+                JSON,
+                "[{\"a\":1,\"b\":2},{\"three\":3,\"none\":null},{},{\"h1\":null,\"h2\":null},null]");
+        assertFunction(
+                "cast(ARRAY[ROW(1, 2), ROW(3, CAST(null as INTEGER)), CAST(ROW(null, null) AS ROW(INTEGER, INTEGER)), null] AS JSON)",
+                JSON,
+                "[[1,2],[3,null],[null,null],null]");
+    }
+
+    @Test
+    public void testCastFromMap()
+    {
+        assertFunction("cast(cast (null as MAP<BIGINT, BIGINT>) AS JSON)", JSON, null);
+        assertFunction("cast(MAP() AS JSON)", JSON, "{}");
+        assertFunction("cast(MAP(ARRAY[1, 2], ARRAY[null, null]) AS JSON)", JSON, "{\"1\":null,\"2\":null}");
+
+        // Test key types
+        assertFunction("CAST(MAP(ARRAY[true, false], ARRAY[1, 2]) AS JSON)", JSON, "{\"false\":2,\"true\":1}");
+
+        assertFunction(
+                "cast(MAP(cast(ARRAY[1, 2, 3] AS ARRAY<TINYINT>), ARRAY[5, 8, null]) AS JSON)",
+                JSON,
+                "{\"1\":5,\"2\":8,\"3\":null}");
+        assertFunction(
+                "cast(MAP(cast(ARRAY[12345, 12346, 12347] AS ARRAY<SMALLINT>), ARRAY[5, 8, null]) AS JSON)",
+                JSON,
+                "{\"12345\":5,\"12346\":8,\"12347\":null}");
+        assertFunction(
+                "cast(MAP(cast(ARRAY[123456789,123456790,123456791] AS ARRAY<INTEGER>), ARRAY[5, 8, null]) AS JSON)",
+                JSON,
+                "{\"123456789\":5,\"123456790\":8,\"123456791\":null}");
+        assertFunction(
+                "cast(MAP(cast(ARRAY[1234567890123456111,1234567890123456222,1234567890123456777] AS ARRAY<BIGINT>), ARRAY[111, 222, null]) AS JSON)",
+                JSON,
+                "{\"1234567890123456111\":111,\"1234567890123456222\":222,\"1234567890123456777\":null}");
+
+        assertFunction(
+                "cast(MAP(cast(ARRAY[3.14, 1e10, 1e20] AS ARRAY<REAL>), ARRAY[null, 10, 20]) AS JSON)",
+                JSON,
+                "{\"1.0E10\":10,\"1.0E20\":20,\"3.14\":null}");
+
+        assertFunction(
+                "cast(MAP(ARRAY[1e-323,1e308,nan()], ARRAY[-323,308,null]) AS JSON)",
+                JSON,
+                "{\"1.0E-323\":-323,\"1.0E308\":308,\"NaN\":null}");
+        assertFunction(
+                "cast(MAP(ARRAY[DECIMAL '3.14', DECIMAL '0.01'], ARRAY[0.14, null]) AS JSON)",
+                JSON,
+                "{\"0.01\":null,\"3.14\":0.14}");
+
+        assertFunction(
+                "cast(MAP(ARRAY[DECIMAL '12345678901234567890.1234567890666666', DECIMAL '0.0'], ARRAY[666666, null]) AS JSON)",
+                JSON,
+                "{\"0E-16\":null,\"12345678901234567890.1234567890666666\":666666}");
+
+        assertFunction("CAST(MAP(ARRAY['a', 'bb', 'ccc'], ARRAY[1, 2, 3]) AS JSON)", JSON, "{\"a\":1,\"bb\":2,\"ccc\":3}");
+
+        // Test value types
+        assertFunction("cast(MAP(ARRAY[1, 2, 3], ARRAY[true, false, null]) AS JSON)", JSON, "{\"1\":true,\"2\":false,\"3\":null}");
+
+        assertFunction(
+                "cast(MAP(ARRAY[1, 2, 3], cast(ARRAY[5, 8, null] AS ARRAY<TINYINT>)) AS JSON)",
+                JSON,
+                "{\"1\":5,\"2\":8,\"3\":null}");
+        assertFunction(
+                "cast(MAP(ARRAY[1, 2, 3], cast(ARRAY[12345, -12345, null] AS ARRAY<SMALLINT>)) AS JSON)",
+                JSON,
+                "{\"1\":12345,\"2\":-12345,\"3\":null}");
+        assertFunction(
+                "cast(MAP(ARRAY[1, 2, 3], cast(ARRAY[123456789, -123456789, null] AS ARRAY<INTEGER>)) AS JSON)",
+                JSON,
+                "{\"1\":123456789,\"2\":-123456789,\"3\":null}");
+        assertFunction(
+                "cast(MAP(ARRAY[1, 2, 3], cast(ARRAY[1234567890123456789, -1234567890123456789, null] AS ARRAY<BIGINT>)) AS JSON)",
+                JSON,
+                "{\"1\":1234567890123456789,\"2\":-1234567890123456789,\"3\":null}");
+
+        assertFunction(
+                "CAST(MAP(ARRAY[1, 2, 3, 5, 8], CAST(ARRAY[3.14, nan(), infinity(), -infinity(), null] AS ARRAY<REAL>)) AS JSON)",
+                JSON,
+                "{\"1\":3.14,\"2\":\"NaN\",\"3\":\"Infinity\",\"5\":\"-Infinity\",\"8\":null}");
+        assertFunction(
+                "CAST(MAP(ARRAY[1, 2, 3, 5, 8, 13, 21], ARRAY[3.14, 1e-323, 1e308, nan(), infinity(), -infinity(), null]) AS JSON)",
+                JSON,
+                "{\"1\":3.14,\"13\":\"-Infinity\",\"2\":1.0E-323,\"21\":null,\"3\":1.0E308,\"5\":\"NaN\",\"8\":\"Infinity\"}");
+        assertFunction("CAST(MAP(ARRAY[1, 2], ARRAY[DECIMAL '3.14', null]) AS JSON)", JSON, "{\"1\":3.14,\"2\":null}");
+        assertFunction(
+                "CAST(MAP(ARRAY[1, 2], ARRAY[DECIMAL '12345678901234567890.123456789012345678', null]) AS JSON)",
+                JSON,
+                "{\"1\":12345678901234567890.123456789012345678,\"2\":null}");
+
+        assertFunction("cast(MAP(ARRAY[1, 2, 3], ARRAY['a', 'bb', null]) AS JSON)", JSON, "{\"1\":\"a\",\"2\":\"bb\",\"3\":null}");
+        assertFunction(
+                "CAST(MAP(ARRAY[1, 2, 3, 5, 8, 13, 21, 34], ARRAY[JSON '123', JSON '3.14', JSON 'false', JSON '\"abc\"', JSON '[1, \"a\", null]', JSON '{\"a\": 1, \"b\": \"str\", \"c\": null}', JSON 'null', null]) AS JSON)",
+                JSON,
+                "{\"1\":123,\"13\":{\"a\":1,\"b\":\"str\",\"c\":null},\"2\":3.14,\"21\":null,\"3\":false,\"34\":null,\"5\":\"abc\",\"8\":[1,\"a\",null]}");
+
+        assertFunction(
+                "cast(MAP(ARRAY[1, 2, 3, 5, 8], ARRAY[ARRAY[1, 2], ARRAY[3, null], ARRAY[], ARRAY[null, null], null]) AS JSON)",
+                JSON,
+                "{\"1\":[1,2],\"2\":[3,null],\"3\":[],\"5\":[null,null],\"8\":null}");
+        assertFunction(
+                "cast(MAP(ARRAY[1, 2, 3, 5, 8], ARRAY[MAP(ARRAY['a', 'b'], ARRAY[1, 2]), MAP(ARRAY['three', 'none'], ARRAY[3, null]), MAP(), MAP(ARRAY['h1', 'h2'], ARRAY[null, null]), null]) AS JSON)",
+                JSON,
+                "{\"1\":{\"a\":1,\"b\":2},\"2\":{\"none\":null,\"three\":3},\"3\":{},\"5\":{\"h1\":null,\"h2\":null},\"8\":null}");
+        assertFunction(
+                "cast(MAP(ARRAY[1, 2, 3, 5], ARRAY[ROW(1, 2), ROW(3, CAST(null as INTEGER)), CAST(ROW(null, null) AS ROW(INTEGER, INTEGER)), null]) AS JSON)",
+                JSON,
+                "{\"1\":[1,2],\"2\":[3,null],\"3\":[null,null],\"5\":null}");
+    }
+
+    @Test
+    public void testCastFromRow()
+    {
+        assertFunction("cast(cast (null as ROW(BIGINT, VARCHAR)) AS JSON)", JSON, null);
+        assertFunction("cast(ROW(null, null) as json)", JSON, "[null,null]");
+
+        assertFunction("cast(ROW(true, false, null) AS JSON)", JSON, "[true,false,null]");
+
+        assertFunction(
+                "cast(cast(ROW(12, 12345, 123456789, 1234567890123456789, null, null, null, null) AS ROW(TINYINT, SMALLINT, INTEGER, BIGINT, TINYINT, SMALLINT, INTEGER, BIGINT)) AS JSON)",
+                JSON,
+                "[12,12345,123456789,1234567890123456789,null,null,null,null]");
+
+        assertFunction(
+                "CAST(ROW(CAST(3.14 AS REAL), 3.1415, 1e308, DECIMAL '3.14', DECIMAL '12345678901234567890.123456789012345678', CAST(null AS REAL), CAST(null AS DOUBLE), CAST(null AS DECIMAL)) AS JSON)",
+                JSON,
+                "[3.14,3.1415,1.0E308,3.14,12345678901234567890.123456789012345678,null,null,null]"
+        );
+
+        assertFunction(
+                "CAST(ROW('a', 'bb', CAST(null as VARCHAR), JSON '123', JSON '3.14', JSON 'false', JSON '\"abc\"', JSON '[1, \"a\", null]', JSON '{\"a\": 1, \"b\": \"str\", \"c\": null}', JSON 'null', CAST(null AS JSON)) AS JSON)",
+                JSON,
+                "[\"a\",\"bb\",null,123,3.14,false,\"abc\",[1,\"a\",null],{\"a\":1,\"b\":\"str\",\"c\":null},null,null]");
+
+        assertFunction(
+                "cast(ROW(ARRAY[1, 2], ARRAY[3, null], ARRAY[], ARRAY[null, null], CAST(null AS ARRAY<BIGINT>)) AS JSON)",
+                JSON,
+                "[[1,2],[3,null],[],[null,null],null]");
+        assertFunction(
+                "cast(ROW(MAP(ARRAY['a', 'b'], ARRAY[1, 2]), MAP(ARRAY['three', 'none'], ARRAY[3, null]), MAP(), MAP(ARRAY['h1', 'h2'], ARRAY[null, null]), CAST(NULL as MAP<VARCHAR, BIGINT>)) AS JSON)",
+                JSON,
+                "[{\"a\":1,\"b\":2},{\"three\":3,\"none\":null},{},{\"h1\":null,\"h2\":null},null]");
+        assertFunction(
+                "cast(ROW(ROW(1, 2), ROW(3, CAST(null as INTEGER)), CAST(ROW(null, null) AS ROW(INTEGER, INTEGER)), null) AS JSON)",
+                JSON,
+                "[[1,2],[3,null],[null,null],null]");
+    }
 }
