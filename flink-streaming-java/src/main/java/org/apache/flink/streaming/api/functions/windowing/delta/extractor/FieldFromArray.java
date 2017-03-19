d65502aaf1d5c56e5d87b84b46a0e0ed659b192d
Aljoscha Krettek <aljoscha.krettek@gmail.com>
2017-03-19 11:17:47 +0100

    [FLINK-6107] Enable trailing whitespace check in streaming checkstyle

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/api/functions/windowing/delta/extractor/FieldFromArray.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/api/functions/windowing/delta/extractor/FieldFromArray.java
@@ -23,7 +23,7 @@ import java.lang.reflect.Array;
 
 /**
  * Extracts a single field out of an array.
- * 
+ *
  * @param <OUT>
  *            The type of the extracted field.
  */
@@ -43,7 +43,7 @@ public class FieldFromArray<OUT> implements Extractor<Object, OUT> {
 
 	/**
 	 * Extracts the field with the given id from the array.
-	 * 
+	 *
 	 * @param fieldId
 	 *            The id of the field which will be extracted from the array.
 	 */
