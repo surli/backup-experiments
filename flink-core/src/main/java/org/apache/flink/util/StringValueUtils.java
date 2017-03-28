59149dc53fd72d23a6a1bfa8faca1d04f2201a1e
Stephan Ewen <sewen@apache.org>
2017-03-28 15:09:02 +0200

    [FLINK-6338] [core] Rename SimpleStringUtils to StringValueUtils
    This more intuitivce name clarifies the role of the class.
    This also marks the class as PublicEvolving, as it is not actually used as an internal class.

--- flink-core/src/main/java/org/apache/flink/util/StringValueUtils.java
+++ flink-core/src/main/java/org/apache/flink/util/StringValueUtils.java
@@ -20,31 +20,34 @@ package org.apache.flink.util;
 
 import java.io.Serializable;
 
-import org.apache.flink.annotation.Internal;
+import org.apache.flink.annotation.PublicEvolving;
 import org.apache.flink.types.StringValue;
 
 /**
- * Utility class for efficient string operations on strings. All methods in this class are
- * written to be optimized for efficiency and work only on strings whose characters are
- * representable in a single <tt>char</tt>, ie. strings without surrogate characters.
+ * Utility class for efficient operations on {@link StringValue}.
+ *
+ * <p>All methods in this class are written to be optimized for efficiency and work directly
+ * on the StringValues char arrays, avoiding copies. For simplicity and efficiency, the methods
+ * only apply to strings whose characters are representable in a single <tt>char</tt>,
+ * ie. strings without surrogate characters.
  */
-@Internal
-public final class SimpleStringUtils {
-	
+@PublicEvolving
+public final class StringValueUtils {
+
 	/**
 	 * Converts the given <code>StringValue</code> into a lower case variant.
-	 * 
+	 *
 	 * @param string The string to convert to lower case.
 	 */
 	public static void toLowerCase(StringValue string) {
 		final char[] chars = string.getCharArray();
 		final int len = string.length();
-		
+
 		for (int i = 0; i < len; i++) {
 			chars[i] = Character.toLowerCase(chars[i]);
 		}
 	}
-	
+
 	/**
 	 * Replaces all non-word characters in a string by a given character. The only
 	 * characters not replaced are the characters that qualify as word characters
@@ -52,14 +55,14 @@ public final class SimpleStringUtils {
 	 * {@link Character#isDigit(char)}, as well as the underscore character.
 	 * <p>
 	 * This operation is intended to simplify strings for counting distinct words.
-	 * 
+	 *
 	 * @param string The string value to have the non-word characters replaced.
 	 * @param replacement The character to use as the replacement.
 	 */
 	public static void replaceNonWordChars(StringValue string, char replacement) {
 		final char[] chars = string.getCharArray();
 		final int len = string.length();
-		
+
 		for (int i = 0; i < len; i++) {
 			final char c = chars[i];
 			if (!(Character.isLetter(c) || Character.isDigit(c) || c == '_')) {
@@ -67,9 +70,9 @@ public final class SimpleStringUtils {
 			}
 		}
 	}
-	
+
 	// ============================================================================================
-	
+
 	/**
 	 * A tokenizer for string values that uses whitespace characters as token delimiters.
 	 * The tokenizer is designed to have a resettable state and operate on mutable objects,
@@ -77,19 +80,19 @@ public final class SimpleStringUtils {
 	 */
 	public static final class WhitespaceTokenizer implements Serializable {
 		private static final long serialVersionUID = 1L;
-		
+
 		private StringValue toTokenize;		// the string to tokenize
 		private int pos;					// the current position in the string
 		private int limit;					// the limit in the string's character data
-		
+
 		/**
 		 * Creates a new tokenizer with an undefined internal state.
 		 */
 		public WhitespaceTokenizer() {}
-		
+
 		/**
 		 * Sets the string to be tokenized and resets the state of the tokenizer.
-		 * 
+		 *
 		 * @param string The string value to be tokenized.
 		 */
 		public void setStringToTokenize(StringValue string) {
@@ -97,11 +100,11 @@ public final class SimpleStringUtils {
 			this.pos = 0;
 			this.limit = string.length();
 		}
-		
+
 		/**
 		 * Gets the next token from the string. If another token is available, the token is stored
 		 * in the given target StringValue object.
-		 * 
+		 *
 		 * @param target The StringValue object to store the next token in.
 		 * @return True, if there was another token, false if not.
 		 */
@@ -109,28 +112,28 @@ public final class SimpleStringUtils {
 			final char[] data = this.toTokenize.getCharArray();
 			final int limit = this.limit;
 			int pos = this.pos;
-			
+
 			// skip the delimiter
 			for (; pos < limit && Character.isWhitespace(data[pos]); pos++);
-			
+
 			if (pos >= limit) {
 				this.pos = pos;
 				return false;
 			}
-			
+
 			final int start = pos;
 			for (; pos < limit && !Character.isWhitespace(data[pos]); pos++);
-			
+
 			this.pos = pos;
 			target.setValue(this.toTokenize, start, pos - start);
 			return true;
 		}
 	}
-	
+
 	// ============================================================================================
-	
+
 	/**
 	 * Private constructor to prevent instantiation, as this is a utility method encapsulating class.
 	 */
-	private SimpleStringUtils() {}
+	private StringValueUtils() {}
 }
