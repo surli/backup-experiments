cf15ea732d4ef809c9275de8771d8d76258b6e5a
Greg Hogan <code@greghogan.com>
2017-03-31 13:07:35 -0400

    Update tests and run sorter tests with HeapSort and with object reuse disabled

--- flink-core/src/test/java/org/apache/flink/api/common/typeutils/SerializerTestBase.java
+++ flink-core/src/test/java/org/apache/flink/api/common/typeutils/SerializerTestBase.java
@@ -18,11 +18,13 @@
 
 package org.apache.flink.api.common.typeutils;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import org.apache.commons.lang3.SerializationException;
+import org.apache.commons.lang3.SerializationUtils;
+import org.apache.flink.core.memory.DataInputView;
+import org.apache.flink.core.memory.DataOutputView;
+import org.apache.flink.util.TestLogger;
+import org.junit.Assert;
+import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -31,14 +33,12 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
 
-import org.apache.flink.util.TestLogger;
-import org.junit.Assert;
-
-import org.apache.commons.lang3.SerializationException;
-import org.apache.commons.lang3.SerializationUtils;
-import org.apache.flink.core.memory.DataInputView;
-import org.apache.flink.core.memory.DataOutputView;
-import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 /**
  * Abstract test base for serializers.
@@ -109,6 +109,11 @@ public abstract class SerializerTestBase<T> extends TestLogger {
 				T copy = serializer.copy(datum);
 				copy.toString();
 				deepEquals("Copied element is not equal to the original element.", datum, copy);
+
+				if (!serializer.isImmutableType()) {
+					assertNotSame("Mutable object serializers must create a deep copy.",
+						copy, datum);
+				}
 			}
 		}
 		catch (Exception e) {
@@ -128,6 +133,11 @@ public abstract class SerializerTestBase<T> extends TestLogger {
 				T copy = serializer.copy(datum, serializer.createInstance());
 				copy.toString();
 				deepEquals("Copied element is not equal to the original element.", datum, copy);
+
+				if (!serializer.isImmutableType()) {
+					assertNotSame("Mutable object serializers must create a deep copy.",
+						copy, datum);
+				}
 			}
 		}
 		catch (Exception e) {
