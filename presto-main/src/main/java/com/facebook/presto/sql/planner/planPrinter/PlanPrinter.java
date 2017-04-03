prestodb/presto

Pass types to CostCalculator

presto-main/src/main/java/com/facebook/presto/sql/planner/PlanPrinter.java
@@ -142,7 +142,7 @@ public class PlanPrinter
         this.metadata = metadata;
         this.stats = Optional.empty();
 
-        Map<PlanNode, PlanNodeCost> costs = costCalculator.calculateCostForPlan(session, plan);
+        Map<PlanNode, PlanNodeCost> costs = costCalculator.calculateCostForPlan(session, types, plan);
         Visitor visitor = new Visitor(types, costs, session);
         plan.accept(visitor, indent);
     }
@@ -157,7 +157,7 @@ public class PlanPrinter
         this.metadata = metadata;
         this.stats = Optional.of(stats);
 
-        Map<PlanNode, PlanNodeCost> costs = costCalculator.calculateCostForPlan(session, plan);
+        Map<PlanNode, PlanNodeCost> costs = costCalculator.calculateCostForPlan(session, types, plan);
         Visitor visitor = new Visitor(types, costs, session);
         plan.accept(visitor, indent);
     }

surli/backup-experiments

presto-main/src/main/java/com/facebook/presto/sql/planner/planPrinter/PlanPrinter.java
@@ -148,7 +148,7 @@ public class PlanPrinter
         this.metadata = metadata;
         this.stats = Optional.empty();
 
-        Map<PlanNodeId, PlanNodeCost> costs = costCalculator.calculateCostForPlan(session, plan);
+        Map<PlanNodeId, PlanNodeCost> costs = costCalculator.calculateCostForPlan(session, types, plan);
         Visitor visitor = new Visitor(types, costs, session);
         plan.accept(visitor, indent);
     }
@@ -163,7 +163,7 @@ public class PlanPrinter
         this.metadata = metadata;
         this.stats = Optional.of(stats);
 
-        Map<PlanNodeId, PlanNodeCost> costs = costCalculator.calculateCostForPlan(session, plan);
+        Map<PlanNodeId, PlanNodeCost> costs = costCalculator.calculateCostForPlan(session, types, plan);
         Visitor visitor = new Visitor(types, costs, session);
         plan.accept(visitor, indent);
     }