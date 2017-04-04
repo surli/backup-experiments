smdorval/presto

Implement limits for spilled data size

presto-main/src/main/java/com/facebook/presto/spiller/LocalSpillContext.java
@@ -11,18 +11,28 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.facebook.presto.spiller;
 
-import com.facebook.presto.spi.type.Type;
-import org.weakref.jmx.Managed;
+import com.facebook.presto.operator.AbstractOperatorSpillContext;
 
-import java.util.List;
-
-public interface SpillerFactory
+public class LocalSpillContext
 {
-    Spiller create(List<Type> types, LocalSpillContext localSpillContext);
+    private final AbstractOperatorSpillContext operatorSpillContext;
+    private long spilledBytes;
 
-    @Managed
-    long getTotalSpilledBytes();
+    public LocalSpillContext(AbstractOperatorSpillContext operatorSpillContext)
+    {
+        this.operatorSpillContext = operatorSpillContext;
+    }
+
+    public void updateBytes(long bytes)
+    {
+        operatorSpillContext.updateBytes(bytes);
+        spilledBytes += bytes;
+    }
+
+    public void close()
+    {
+        operatorSpillContext.updateBytes(-spilledBytes);
+    }
 }

presto-main/src/main/java/com/facebook/presto/spiller/LocalSpillManager.java
@@ -11,18 +11,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.facebook.presto.spiller;
 
-import com.facebook.presto.spi.type.Type;
-import org.weakref.jmx.Managed;
+import com.google.inject.Inject;
 
-import java.util.List;
+import static java.util.Objects.requireNonNull;
 
-public interface SpillerFactory
+public final class LocalSpillManager
 {
-    Spiller create(List<Type> types, LocalSpillContext localSpillContext);
+    private final SpillSpaceTracker spillSpaceTracker;
 
-    @Managed
-    long getTotalSpilledBytes();
+    @Inject
+    public LocalSpillManager(NodeSpillConfig nodeSpillConfig)
+    {
+        requireNonNull(nodeSpillConfig, "config is null");
+        spillSpaceTracker = new SpillSpaceTracker(nodeSpillConfig.getMaxSpillPerNode());
+    }
+
+    public SpillSpaceTracker getSpillSpaceTracker()
+    {
+        return spillSpaceTracker;
+    }
 }

surli/backup-experiments

presto-main/src/main/java/com/facebook/presto/spiller/LocalSpillContext.java
@@ -11,19 +11,30 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.facebook.presto.spiller;
 
 import com.facebook.presto.operator.SpillContext;
-import com.facebook.presto.spi.type.Type;
-import org.weakref.jmx.Managed;
 
-import java.util.List;
-
-public interface SpillerFactory
+public class LocalSpillContext
+    implements SpillContext
 {
-    Spiller create(List<Type> types, SpillContext localSpillContext);
+    private final SpillContext parentSpillContext;
+    private long spilledBytes;
 
-    @Managed
-    long getTotalSpilledBytes();
+    public LocalSpillContext(SpillContext parentSpillContext)
+    {
+        this.parentSpillContext = parentSpillContext;
+    }
+
+    @Override
+    public void updateBytes(long bytes)
+    {
+        parentSpillContext.updateBytes(bytes);
+        spilledBytes += bytes;
+    }
+
+    public void close()
+    {
+        parentSpillContext.updateBytes(-spilledBytes);
+    }
 }

presto-main/src/main/java/com/facebook/presto/spiller/LocalSpillManager.java
@@ -11,19 +11,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.facebook.presto.spiller;
 
-import com.facebook.presto.operator.SpillContext;
-import com.facebook.presto.spi.type.Type;
-import org.weakref.jmx.Managed;
+import com.google.inject.Inject;
 
-import java.util.List;
+import static java.util.Objects.requireNonNull;
 
-public interface SpillerFactory
+public final class LocalSpillManager
 {
-    Spiller create(List<Type> types, SpillContext localSpillContext);
+    private final SpillSpaceTracker spillSpaceTracker;
 
-    @Managed
-    long getTotalSpilledBytes();
+    @Inject
+    public LocalSpillManager(NodeSpillConfig config)
+    {
+        requireNonNull(config, "config is null");
+        spillSpaceTracker = new SpillSpaceTracker(config.getMaxSpillPerNode());
+    }
+
+    public SpillSpaceTracker getSpillSpaceTracker()
+    {
+        return spillSpaceTracker;
+    }
 }