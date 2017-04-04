smdorval/presto

Intoduce stream-spiller based GenericSpiller
Add concept of SingleStreamSpiller which is responsible of
spilling single stream of data.
A set of single stream spiller is wrapped with in GenericSpiller class.
Old BinaryFileSpiller is replaced with GenericSpill parametrized with
BinaryFileSingleStreamSpiller factory.

presto-main/src/main/java/com/facebook/presto/testing/LocalQueryRunner.java
@@ -95,7 +95,8 @@ import com.facebook.presto.spi.block.BlockBuilder;
 import com.facebook.presto.spi.block.BlockEncodingSerde;
 import com.facebook.presto.spi.connector.ConnectorFactory;
 import com.facebook.presto.spi.type.Type;
-import com.facebook.presto.spiller.BinarySpillerFactory;
+import com.facebook.presto.spiller.BinaryFileSingleStreamSpillerFactory;
+import com.facebook.presto.spiller.GenericSpillerFactory;
 import com.facebook.presto.spiller.SpillerFactory;
 import com.facebook.presto.spiller.SpillerStats;
 import com.facebook.presto.split.PageSinkManager;
@@ -358,7 +359,7 @@ public class LocalQueryRunner
                 .build();
 
         SpillerStats spillerStats = new SpillerStats();
-        this.spillerFactory = new BinarySpillerFactory(blockEncodingSerde, spillerStats, featuresConfig);
+        this.spillerFactory = new GenericSpillerFactory(new BinaryFileSingleStreamSpillerFactory(blockEncodingSerde, spillerStats, featuresConfig));
     }
 
     public static LocalQueryRunner queryRunnerWithInitialTransaction(Session defaultSession)

surli/backup-experiments

presto-main/src/main/java/com/facebook/presto/testing/LocalQueryRunner.java
@@ -95,7 +95,8 @@ import com.facebook.presto.spi.block.BlockBuilder;
 import com.facebook.presto.spi.block.BlockEncodingSerde;
 import com.facebook.presto.spi.connector.ConnectorFactory;
 import com.facebook.presto.spi.type.Type;
-import com.facebook.presto.spiller.BinarySpillerFactory;
+import com.facebook.presto.spiller.FileSingleStreamSpillerFactory;
+import com.facebook.presto.spiller.GenericSpillerFactory;
 import com.facebook.presto.spiller.SpillerFactory;
 import com.facebook.presto.spiller.SpillerStats;
 import com.facebook.presto.split.PageSinkManager;
@@ -358,7 +359,7 @@ public class LocalQueryRunner
                 .build();
 
         SpillerStats spillerStats = new SpillerStats();
-        this.spillerFactory = new BinarySpillerFactory(blockEncodingSerde, spillerStats, featuresConfig);
+        this.spillerFactory = new GenericSpillerFactory(new FileSingleStreamSpillerFactory(blockEncodingSerde, spillerStats, featuresConfig));
     }
 
     public static LocalQueryRunner queryRunnerWithInitialTransaction(Session defaultSession)