smdorval/presto

Extract SpillerStats out of SpillerFactoryWithStats
Preparatory refactor

presto-main/src/main/java/com/facebook/presto/spiller/BinarySpillerFactory.java
@@ -33,28 +33,31 @@ import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.Executors.newFixedThreadPool;
 
 public class BinarySpillerFactory
-        extends SpillerFactoryWithStats
+        implements SpillerFactory
 {
     public static final String SPILLER_THREAD_NAME_PREFIX = "binary-spiller";
 
     private final ListeningExecutorService executor;
     private final PagesSerdeFactory serde;
     private final Path spillPath;
+    private final SpillerStats spillerStats;
 
     @Inject
-    public BinarySpillerFactory(BlockEncodingSerde blockEncodingSerde, FeaturesConfig featuresConfig)
+    public BinarySpillerFactory(BlockEncodingSerde blockEncodingSerde, SpillerStats spillerStats, FeaturesConfig featuresConfig)
     {
         this(createExecutorServiceOfSize(requireNonNull(featuresConfig, "featuresConfig is null").getSpillerThreads()),
                 blockEncodingSerde,
+                spillerStats,
                 requireNonNull(featuresConfig, "featuresConfig is null").getSpillerSpillPath());
     }
 
-    public BinarySpillerFactory(ListeningExecutorService executor, BlockEncodingSerde blockEncodingSerde, Path spillPath)
+    public BinarySpillerFactory(ListeningExecutorService executor, BlockEncodingSerde blockEncodingSerde, SpillerStats spillerStats, Path spillPath)
     {
         requireNonNull(blockEncodingSerde, "blockEncodingSerde is null");
         // TODO: add separate config for spill compression
         this.serde = new PagesSerdeFactory(blockEncodingSerde, false);
         this.executor = requireNonNull(executor, "executor is null");
+        this.spillerStats = requireNonNull(spillerStats, "spillerStats can not be null");
         this.spillPath = requireNonNull(spillPath, "spillPath is null");
         this.spillPath.toFile().mkdirs();
     }
@@ -69,6 +72,6 @@ public class BinarySpillerFactory
     @Override
     public Spiller create(List<Type> types, LocalSpillContext localSpillContext)
     {
-        return new BinaryFileSpiller(serde.createPagesSerde(), executor, spillPath, totalSpilledBytes, localSpillContext);
+        return new BinaryFileSpiller(serde.createPagesSerde(), executor, spillPath, spillerStats, localSpillContext);
     }
 }

surli/backup-experiments

presto-main/src/main/java/com/facebook/presto/spiller/BinarySpillerFactory.java
@@ -34,28 +34,31 @@ import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.Executors.newFixedThreadPool;
 
 public class BinarySpillerFactory
-        extends SpillerFactoryWithStats
+        implements SpillerFactory
 {
     public static final String SPILLER_THREAD_NAME_PREFIX = "binary-spiller";
 
     private final ListeningExecutorService executor;
     private final PagesSerdeFactory serde;
     private final Path spillPath;
+    private final SpillerStats spillerStats;
 
     @Inject
-    public BinarySpillerFactory(BlockEncodingSerde blockEncodingSerde, FeaturesConfig featuresConfig)
+    public BinarySpillerFactory(BlockEncodingSerde blockEncodingSerde, SpillerStats spillerStats, FeaturesConfig featuresConfig)
     {
         this(createExecutorServiceOfSize(requireNonNull(featuresConfig, "featuresConfig is null").getSpillerThreads()),
                 blockEncodingSerde,
+                spillerStats,
                 requireNonNull(featuresConfig, "featuresConfig is null").getSpillerSpillPath());
     }
 
-    public BinarySpillerFactory(ListeningExecutorService executor, BlockEncodingSerde blockEncodingSerde, Path spillPath)
+    public BinarySpillerFactory(ListeningExecutorService executor, BlockEncodingSerde blockEncodingSerde, SpillerStats spillerStats, Path spillPath)
     {
         requireNonNull(blockEncodingSerde, "blockEncodingSerde is null");
         // TODO: add separate config for spill compression
         this.serde = new PagesSerdeFactory(blockEncodingSerde, false);
         this.executor = requireNonNull(executor, "executor is null");
+        this.spillerStats = requireNonNull(spillerStats, "spillerStats can not be null");
         this.spillPath = requireNonNull(spillPath, "spillPath is null");
         this.spillPath.toFile().mkdirs();
     }
@@ -70,6 +73,6 @@ public class BinarySpillerFactory
     @Override
     public Spiller create(List<Type> types, SpillContext spillContext)
     {
-        return new BinaryFileSpiller(serde.createPagesSerde(), executor, spillPath, totalSpilledBytes, spillContext);
+        return new BinaryFileSpiller(serde.createPagesSerde(), executor, spillPath, spillerStats, spillContext);
     }
 }