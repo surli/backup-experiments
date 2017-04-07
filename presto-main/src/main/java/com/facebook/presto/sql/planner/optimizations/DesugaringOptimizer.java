prestodb/presto

Add missing plan nodes support to DesugaringOptimizer

presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/DesugaringOptimizer.java
@@ -22,20 +22,30 @@ import com.facebook.presto.sql.planner.LambdaCaptureDesugaringRewriter;
 import com.facebook.presto.sql.planner.PlanNodeIdAllocator;
 import com.facebook.presto.sql.planner.Symbol;
 import com.facebook.presto.sql.planner.SymbolAllocator;
+import com.facebook.presto.sql.planner.plan.AggregationNode;
+import com.facebook.presto.sql.planner.plan.AggregationNode.Aggregation;
+import com.facebook.presto.sql.planner.plan.ApplyNode;
 import com.facebook.presto.sql.planner.plan.Assignments;
 import com.facebook.presto.sql.planner.plan.FilterNode;
+import com.facebook.presto.sql.planner.plan.JoinNode;
 import com.facebook.presto.sql.planner.plan.PlanNode;
 import com.facebook.presto.sql.planner.plan.ProjectNode;
 import com.facebook.presto.sql.planner.plan.SimplePlanRewriter;
 import com.facebook.presto.sql.planner.plan.TableScanNode;
+import com.facebook.presto.sql.planner.plan.ValuesNode;
 import com.facebook.presto.sql.tree.Expression;
 import com.facebook.presto.sql.tree.ExpressionTreeRewriter;
+import com.facebook.presto.sql.tree.FunctionCall;
 import com.facebook.presto.sql.tree.SymbolReference;
 import com.facebook.presto.util.maps.IdentityLinkedHashMap;
 
 import java.util.Map;
 
 import static com.facebook.presto.sql.analyzer.ExpressionAnalyzer.getExpressionTypes;
+import static com.facebook.presto.sql.planner.ExpressionExtractor.extractExpressionsNonRecursive;
+import static com.facebook.presto.util.ImmutableCollectors.toImmutableList;
+import static com.facebook.presto.util.ImmutableCollectors.toImmutableMap;
+import static com.google.common.base.Preconditions.checkState;
 import static java.util.Collections.emptyList;
 import static java.util.Objects.requireNonNull;
 
@@ -81,6 +91,32 @@ public class DesugaringOptimizer
         }
 
         @Override
+        public PlanNode visitPlan(PlanNode node, RewriteContext<Void> context)
+        {
+            checkState(extractExpressionsNonRecursive(node).isEmpty(), "Unhandled plan node with expressions");
+            return super.visitPlan(node, context);
+        }
+
+        @Override
+        public PlanNode visitAggregation(AggregationNode node, RewriteContext<Void> context)
+        {
+            PlanNode source = context.rewrite(node.getSource());
+            Map<Symbol, Aggregation> assignments = node.getAssignments().entrySet().stream()
+                    .collect(toImmutableMap(Map.Entry::getKey, entry -> {
+                        Aggregation aggregation = entry.getValue();
+                        return new Aggregation((FunctionCall) desugar(aggregation.getCall()), aggregation.getSignature(), aggregation.getMask());
+                    }));
+            return new AggregationNode(
+                    node.getId(),
+                    source,
+                    assignments,
+                    node.getGroupingSets(),
+                    node.getStep(),
+                    node.getHashSymbol(),
+                    node.getGroupIdSymbol());
+        }
+
+        @Override
         public PlanNode visitProject(ProjectNode node, RewriteContext<Void> context)
         {
             PlanNode source = context.rewrite(node.getSource());
@@ -111,6 +147,44 @@ public class DesugaringOptimizer
                     node.getLayout(),
                     node.getCurrentConstraint(),
                     originalConstraint);
+        }
+
+        @Override
+        public PlanNode visitJoin(JoinNode node, RewriteContext<Void> context)
+        {
+            PlanNode left = context.rewrite(node.getLeft());
+            PlanNode right = context.rewrite(node.getRight());
+            return new JoinNode(
+                    node.getId(),
+                    node.getType(),
+                    left,
+                    right,
+                    node.getCriteria(),
+                    node.getOutputSymbols(),
+                    node.getFilter().map(this::desugar),
+                    node.getLeftHashSymbol(),
+                    node.getRightHashSymbol(),
+                    node.getDistributionType());
+        }
+
+        @Override
+        public PlanNode visitValues(ValuesNode node, RewriteContext<Void> context)
+        {
+            return new ValuesNode(node.getId(), node.getOutputSymbols(),
+                    node.getRows().stream()
+                            .map(row -> row.stream()
+                                    .map(this::desugar)
+                                    .collect(toImmutableList()))
+                            .collect(toImmutableList()));
+        }
+
+        @Override
+        public PlanNode visitApply(ApplyNode node, RewriteContext<Void> context)
+        {
+            PlanNode input = context.rewrite(node.getInput());
+            PlanNode subquery = context.rewrite(node.getSubquery());
+            Assignments subqueryAssignments = node.getSubqueryAssignments().rewrite(this::desugar);
+            return new ApplyNode(node.getId(), input, subquery, subqueryAssignments, node.getCorrelation());
         }
 
         private Expression desugar(Expression expression)

surli/backup-experiments

presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/DesugaringOptimizer.java
@@ -22,20 +22,31 @@ import com.facebook.presto.sql.planner.LambdaCaptureDesugaringRewriter;
 import com.facebook.presto.sql.planner.PlanNodeIdAllocator;
 import com.facebook.presto.sql.planner.Symbol;
 import com.facebook.presto.sql.planner.SymbolAllocator;
+import com.facebook.presto.sql.planner.plan.AggregationNode;
+import com.facebook.presto.sql.planner.plan.AggregationNode.Aggregation;
+import com.facebook.presto.sql.planner.plan.ApplyNode;
 import com.facebook.presto.sql.planner.plan.Assignments;
 import com.facebook.presto.sql.planner.plan.FilterNode;
+import com.facebook.presto.sql.planner.plan.JoinNode;
 import com.facebook.presto.sql.planner.plan.PlanNode;
 import com.facebook.presto.sql.planner.plan.ProjectNode;
 import com.facebook.presto.sql.planner.plan.SimplePlanRewriter;
 import com.facebook.presto.sql.planner.plan.TableScanNode;
+import com.facebook.presto.sql.planner.plan.ValuesNode;
 import com.facebook.presto.sql.tree.Expression;
 import com.facebook.presto.sql.tree.ExpressionTreeRewriter;
+import com.facebook.presto.sql.tree.FunctionCall;
 import com.facebook.presto.sql.tree.SymbolReference;
 import com.facebook.presto.util.maps.IdentityLinkedHashMap;
 
 import java.util.Map;
+import java.util.Optional;
 
 import static com.facebook.presto.sql.analyzer.ExpressionAnalyzer.getExpressionTypes;
+import static com.facebook.presto.sql.planner.ExpressionExtractor.extractExpressionsNonRecursive;
+import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static com.google.common.collect.ImmutableMap.toImmutableMap;
 import static java.util.Collections.emptyList;
 import static java.util.Objects.requireNonNull;
 
@@ -81,6 +92,32 @@ public class DesugaringOptimizer
         }
 
         @Override
+        public PlanNode visitPlan(PlanNode node, RewriteContext<Void> context)
+        {
+            checkState(extractExpressionsNonRecursive(node).isEmpty(), "Unhandled plan node with expressions");
+            return super.visitPlan(node, context);
+        }
+
+        @Override
+        public PlanNode visitAggregation(AggregationNode node, RewriteContext<Void> context)
+        {
+            PlanNode source = context.rewrite(node.getSource());
+            Map<Symbol, Aggregation> assignments = node.getAssignments().entrySet().stream()
+                    .collect(toImmutableMap(Map.Entry::getKey, entry -> {
+                        Aggregation aggregation = entry.getValue();
+                        return new Aggregation((FunctionCall) desugar(aggregation.getCall()), aggregation.getSignature(), aggregation.getMask());
+                    }));
+            return new AggregationNode(
+                    node.getId(),
+                    source,
+                    assignments,
+                    node.getGroupingSets(),
+                    node.getStep(),
+                    node.getHashSymbol(),
+                    node.getGroupIdSymbol());
+        }
+
+        @Override
         public PlanNode visitProject(ProjectNode node, RewriteContext<Void> context)
         {
             PlanNode source = context.rewrite(node.getSource());
@@ -111,6 +148,47 @@ public class DesugaringOptimizer
                     node.getLayout(),
                     node.getCurrentConstraint(),
                     originalConstraint);
+        }
+
+        @Override
+        public PlanNode visitJoin(JoinNode node, RewriteContext<Void> context)
+        {
+            PlanNode left = context.rewrite(node.getLeft());
+            PlanNode right = context.rewrite(node.getRight());
+            Optional<Expression> filter = node.getFilter().map(this::desugar);
+            return new JoinNode(
+                    node.getId(),
+                    node.getType(),
+                    left,
+                    right,
+                    node.getCriteria(),
+                    node.getOutputSymbols(),
+                    filter,
+                    node.getLeftHashSymbol(),
+                    node.getRightHashSymbol(),
+                    node.getDistributionType());
+        }
+
+        @Override
+        public PlanNode visitValues(ValuesNode node, RewriteContext<Void> context)
+        {
+            return new ValuesNode(
+                    node.getId(),
+                    node.getOutputSymbols(),
+                    node.getRows().stream()
+                            .map(row -> row.stream()
+                                    .map(this::desugar)
+                                    .collect(toImmutableList()))
+                            .collect(toImmutableList()));
+        }
+
+        @Override
+        public PlanNode visitApply(ApplyNode node, RewriteContext<Void> context)
+        {
+            PlanNode input = context.rewrite(node.getInput());
+            PlanNode subquery = context.rewrite(node.getSubquery());
+            Assignments subqueryAssignments = node.getSubqueryAssignments().rewrite(this::desugar);
+            return new ApplyNode(node.getId(), input, subquery, subqueryAssignments, node.getCorrelation());
         }
 
         private Expression desugar(Expression expression)