725871f7c643b5f208431ce571995d2c61e28ef8
Jochen Schalanda <jochen@graylog.com>
2017-04-10 19:24:58 +0200

    Fix CardinalityAggregationIntegrationTest

---
+++ jest-common/src/main/java/io/searchbox/indices/script/GetStoredScript.java
@@ -0,0 +1,29 @@
+package io.searchbox.indices.script;
+
+/**
+ * @author cihat keser
+ */
+public class GetStoredScript extends AbstractStoredScript {
+    protected GetStoredScript(Builder builder) {
+        super(builder);
+        setURI(buildURI());
+    }
+
+    @Override
+    public String getRestMethodName() {
+        return "GET";
+    }
+
+    public static class Builder extends AbstractStoredScript.Builder<GetStoredScript, Builder> {
+
+        public Builder(String scriptName) {
+            super(scriptName);
+        }
+
+        @Override
+        public GetStoredScript build() {
+            return new GetStoredScript(this);
+        }
+
+    }
+}
