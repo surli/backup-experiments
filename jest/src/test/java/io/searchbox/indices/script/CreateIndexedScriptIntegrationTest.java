ba77870005868868071e5525efe03c854a60073d
Jochen Schalanda <jochen@graylog.com>
2017-04-10 18:32:14 +0200

    Fix CreateIndexIntegrationTest
    See: https://www.elastic.co/guide/en/elasticsearch/reference/5.3/breaking_50_mapping_changes.html#_literal_string_literal_fields_replaced_by_literal_text_literal_literal_keyword_literal_fields

---
+++ jest/src/test/java/io/searchbox/indices/script/CreateIndexedScriptIntegrationTest.java
@@ -0,0 +1,36 @@
+package io.searchbox.indices.script;
+
+import io.searchbox.client.JestResult;
+import io.searchbox.common.AbstractIntegrationTest;
+import org.elasticsearch.action.admin.cluster.storedscripts.GetStoredScriptResponse;
+import org.elasticsearch.test.ESIntegTestCase;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static io.searchbox.indices.script.ScriptLanguage.PAINLESS;
+
+@ESIntegTestCase.ClusterScope(scope = ESIntegTestCase.Scope.TEST, numDataNodes = 1)
+public class CreateIndexedScriptIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    public void createAnIndexedScript() throws IOException {
+        String name = "script-test";
+        String script = "int aVariable = 1; return aVariable";
+
+        CreateStoredScript createIndexedScript = new CreateStoredScript.Builder(name)
+                .setLanguage(PAINLESS)
+                .setSource(script)
+                .build();
+        JestResult result = client.execute(createIndexedScript);
+        assertTrue(result.getErrorMessage(), result.isSucceeded());
+
+        GetStoredScriptResponse getIndexedScriptResponse =
+                client().admin().cluster().prepareGetStoredScript()
+                        .setLang("painless")
+                        .setId(name).get();
+        assertNotNull(getIndexedScriptResponse.getStoredScript());
+        assertEquals(script, getIndexedScriptResponse.getStoredScript());
+    }
+}
+
