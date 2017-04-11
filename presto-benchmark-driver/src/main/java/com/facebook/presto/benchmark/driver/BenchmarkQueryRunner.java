prestodb/presto

Use OkHttp for CLI and JDBC driver

presto-benchmark-driver/src/main/java/com/facebook/presto/benchmark/driver/BenchmarkQueryRunner.java
@@ -15,7 +15,6 @@ package com.facebook.presto.benchmark.driver;
 
 import com.facebook.presto.client.ClientSession;
 import com.facebook.presto.client.QueryError;
-import com.facebook.presto.client.QueryResults;
 import com.facebook.presto.client.StatementClient;
 import com.facebook.presto.client.StatementStats;
 import com.google.common.base.Throwables;
@@ -28,8 +27,8 @@ import io.airlift.http.client.HttpClientConfig;
 import io.airlift.http.client.JsonResponseHandler;
 import io.airlift.http.client.Request;
 import io.airlift.http.client.jetty.JettyHttpClient;
-import io.airlift.json.JsonCodec;
 import io.airlift.units.Duration;
+import okhttp3.OkHttpClient;
 
 import java.io.Closeable;
 import java.net.URI;
@@ -39,6 +38,7 @@ import java.util.concurrent.TimeUnit;
 
 import static com.facebook.presto.benchmark.driver.BenchmarkQueryResult.failResult;
 import static com.facebook.presto.benchmark.driver.BenchmarkQueryResult.passResult;
+import static com.facebook.presto.client.OkHttpUtil.setupSocks;
 import static com.google.common.base.Preconditions.checkArgument;
 import static io.airlift.http.client.HttpUriBuilder.uriBuilderFrom;
 import static io.airlift.http.client.JsonResponseHandler.createJsonResponseHandler;
@@ -59,8 +59,8 @@ public class BenchmarkQueryRunner
     private final int maxFailures;
 
     private final HttpClient httpClient;
+    private final OkHttpClient okHttpClient;
     private final List<URI> nodes;
-    private final JsonCodec<QueryResults> queryResultsCodec;
 
     private int failures;
 
@@ -77,8 +77,6 @@ public class BenchmarkQueryRunner
 
         this.debug = debug;
 
-        this.queryResultsCodec = jsonCodec(QueryResults.class);
-
         requireNonNull(socksProxy, "socksProxy is null");
         HttpClientConfig httpClientConfig = new HttpClientConfig();
         if (socksProxy.isPresent()) {
@@ -86,6 +84,10 @@ public class BenchmarkQueryRunner
         }
 
         this.httpClient = new JettyHttpClient(httpClientConfig.setConnectTimeout(new Duration(10, TimeUnit.SECONDS)));
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        setupSocks(builder, socksProxy);
+        this.okHttpClient = builder.build();
 
         nodes = getAllNodes(requireNonNull(serverUri, "serverUri is null"));
     }
@@ -149,7 +151,7 @@ public class BenchmarkQueryRunner
         failures = 0;
         while (true) {
             // start query
-            StatementClient client = new StatementClient(httpClient, queryResultsCodec, session, "show schemas");
+            StatementClient client = new StatementClient(okHttpClient, session, "show schemas");
 
             // read query output
             ImmutableList.Builder<String> schemas = ImmutableList.builder();
@@ -190,7 +192,7 @@ public class BenchmarkQueryRunner
     private StatementStats execute(ClientSession session, String name, String query)
     {
         // start query
-        StatementClient client = new StatementClient(httpClient, queryResultsCodec, session, query);
+        StatementClient client = new StatementClient(okHttpClient, session, query);
 
         // read query output
         while (client.isValid() && client.advance()) {

presto-jdbc/src/main/java/com/facebook/presto/jdbc/QueryExecutor.java
@@ -14,102 +14,72 @@
 package com.facebook.presto.jdbc;
 
 import com.facebook.presto.client.ClientSession;
-import com.facebook.presto.client.QueryResults;
+import com.facebook.presto.client.JsonResponse;
 import com.facebook.presto.client.ServerInfo;
 import com.facebook.presto.client.StatementClient;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.net.HostAndPort;
-import io.airlift.http.client.HttpClient;
-import io.airlift.http.client.HttpClientConfig;
-import io.airlift.http.client.Request;
-import io.airlift.http.client.jetty.JettyHttpClient;
-import io.airlift.http.client.jetty.JettyIoPool;
-import io.airlift.http.client.jetty.JettyIoPoolConfig;
 import io.airlift.json.JsonCodec;
-import io.airlift.units.Duration;
-
-import javax.annotation.Nullable;
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 import java.io.Closeable;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
-import java.net.ProxySelector;
 import java.net.URI;
-import java.util.concurrent.TimeUnit;
 
-import static io.airlift.http.client.HttpUriBuilder.uriBuilderFrom;
-import static io.airlift.http.client.JsonResponseHandler.createJsonResponseHandler;
-import static io.airlift.http.client.Request.Builder.prepareGet;
+import static com.google.common.net.HttpHeaders.USER_AGENT;
 import static io.airlift.json.JsonCodec.jsonCodec;
 import static java.util.Objects.requireNonNull;
 
 class QueryExecutor
         implements Closeable
 {
-    private final JsonCodec<QueryResults> queryInfoCodec;
-    private final JsonCodec<ServerInfo> serverInfoCodec;
-    private final HttpClient httpClient;
+    private static final JsonCodec<ServerInfo> SERVER_INFO_CODEC = jsonCodec(ServerInfo.class);
 
-    private QueryExecutor(JsonCodec<QueryResults> queryResultsCodec, JsonCodec<ServerInfo> serverInfoCodec, HttpClient httpClient)
+    private final OkHttpClient httpClient;
+
+    private QueryExecutor(OkHttpClient httpClient)
     {
-        this.queryInfoCodec = requireNonNull(queryResultsCodec, "queryResultsCodec is null");
-        this.serverInfoCodec = requireNonNull(serverInfoCodec, "serverInfoCodec is null");
         this.httpClient = requireNonNull(httpClient, "httpClient is null");
     }
 
     public StatementClient startQuery(ClientSession session, String query)
     {
-        return new StatementClient(httpClient, queryInfoCodec, session, query);
+        return new StatementClient(httpClient, session, query);
     }
 
     @Override
     public void close()
     {
-        httpClient.close();
+        httpClient.dispatcher().executorService().shutdown();
+        httpClient.connectionPool().evictAll();
     }
 
     public ServerInfo getServerInfo(URI server)
     {
-        URI uri = uriBuilderFrom(server).replacePath("/v1/info").build();
-        Request request = prepareGet().setUri(uri).build();
-        return httpClient.execute(request, createJsonResponseHandler(serverInfoCodec));
-    }
+        HttpUrl url = HttpUrl.get(server).newBuilder()
+                .encodedPath("/v1/info")
+                .build();
 
-    // TODO: replace this with a phantom reference
-    @SuppressWarnings("FinalizeDeclaration")
-    @Override
-    protected void finalize()
-    {
-        close();
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+
+        return JsonResponse.execute(SERVER_INFO_CODEC, httpClient, request)
+                .getValue();
     }
 
     static QueryExecutor create(String userAgent)
     {
-        return create(new JettyHttpClient(
-                new HttpClientConfig()
-                        .setConnectTimeout(new Duration(10, TimeUnit.SECONDS))
-                        .setSocksProxy(getSystemSocksProxy()),
-                new JettyIoPool("presto-jdbc", new JettyIoPoolConfig()),
-                ImmutableSet.of(new UserAgentRequestFilter(userAgent))));
+        return new QueryExecutor(new OkHttpClient.Builder()
+                .addInterceptor(userAgentInterceptor(userAgent))
+                .build());
     }
 
-    static QueryExecutor create(HttpClient httpClient)
+    private static Interceptor userAgentInterceptor(String userAgent)
     {
-        return new QueryExecutor(jsonCodec(QueryResults.class), jsonCodec(ServerInfo.class), httpClient);
-    }
-
-    @Nullable
-    private static HostAndPort getSystemSocksProxy()
-    {
-        URI uri = URI.create("socket://0.0.0.0:80");
-        for (Proxy proxy : ProxySelector.getDefault().select(uri)) {
-            if (proxy.type() == Proxy.Type.SOCKS) {
-                if (proxy.address() instanceof InetSocketAddress) {
-                    InetSocketAddress address = (InetSocketAddress) proxy.address();
-                    return HostAndPort.fromParts(address.getHostString(), address.getPort());
-                }
-            }
-        }
-        return null;
+        return chain -> chain.proceed(chain.request().newBuilder()
+                .header(USER_AGENT, userAgent)
+                .build());
     }
 }

surli/backup-experiments

presto-benchmark-driver/src/main/java/com/facebook/presto/benchmark/driver/BenchmarkQueryRunner.java
@@ -15,7 +15,6 @@ package com.facebook.presto.benchmark.driver;
 
 import com.facebook.presto.client.ClientSession;
 import com.facebook.presto.client.QueryError;
-import com.facebook.presto.client.QueryResults;
 import com.facebook.presto.client.StatementClient;
 import com.facebook.presto.client.StatementStats;
 import com.google.common.base.Throwables;
@@ -28,8 +27,8 @@ import io.airlift.http.client.HttpClientConfig;
 import io.airlift.http.client.JsonResponseHandler;
 import io.airlift.http.client.Request;
 import io.airlift.http.client.jetty.JettyHttpClient;
-import io.airlift.json.JsonCodec;
 import io.airlift.units.Duration;
+import okhttp3.OkHttpClient;
 
 import java.io.Closeable;
 import java.net.URI;
@@ -39,6 +38,7 @@ import java.util.concurrent.TimeUnit;
 
 import static com.facebook.presto.benchmark.driver.BenchmarkQueryResult.failResult;
 import static com.facebook.presto.benchmark.driver.BenchmarkQueryResult.passResult;
+import static com.facebook.presto.client.OkHttpUtil.setupSocksProxy;
 import static com.google.common.base.Preconditions.checkArgument;
 import static io.airlift.http.client.HttpUriBuilder.uriBuilderFrom;
 import static io.airlift.http.client.JsonResponseHandler.createJsonResponseHandler;
@@ -59,8 +59,8 @@ public class BenchmarkQueryRunner
     private final int maxFailures;
 
     private final HttpClient httpClient;
+    private final OkHttpClient okHttpClient;
     private final List<URI> nodes;
-    private final JsonCodec<QueryResults> queryResultsCodec;
 
     private int failures;
 
@@ -77,8 +77,6 @@ public class BenchmarkQueryRunner
 
         this.debug = debug;
 
-        this.queryResultsCodec = jsonCodec(QueryResults.class);
-
         requireNonNull(socksProxy, "socksProxy is null");
         HttpClientConfig httpClientConfig = new HttpClientConfig();
         if (socksProxy.isPresent()) {
@@ -86,6 +84,10 @@ public class BenchmarkQueryRunner
         }
 
         this.httpClient = new JettyHttpClient(httpClientConfig.setConnectTimeout(new Duration(10, TimeUnit.SECONDS)));
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        setupSocksProxy(builder, socksProxy);
+        this.okHttpClient = builder.build();
 
         nodes = getAllNodes(requireNonNull(serverUri, "serverUri is null"));
     }
@@ -149,7 +151,7 @@ public class BenchmarkQueryRunner
         failures = 0;
         while (true) {
             // start query
-            StatementClient client = new StatementClient(httpClient, queryResultsCodec, session, "show schemas");
+            StatementClient client = new StatementClient(okHttpClient, session, "show schemas");
 
             // read query output
             ImmutableList.Builder<String> schemas = ImmutableList.builder();
@@ -190,7 +192,7 @@ public class BenchmarkQueryRunner
     private StatementStats execute(ClientSession session, String name, String query)
     {
         // start query
-        StatementClient client = new StatementClient(httpClient, queryResultsCodec, session, query);
+        StatementClient client = new StatementClient(okHttpClient, session, query);
 
         // read query output
         while (client.isValid() && client.advance()) {

presto-jdbc/src/main/java/com/facebook/presto/jdbc/QueryExecutor.java
@@ -14,102 +14,60 @@
 package com.facebook.presto.jdbc;
 
 import com.facebook.presto.client.ClientSession;
-import com.facebook.presto.client.QueryResults;
+import com.facebook.presto.client.JsonResponse;
 import com.facebook.presto.client.ServerInfo;
 import com.facebook.presto.client.StatementClient;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.net.HostAndPort;
-import io.airlift.http.client.HttpClient;
-import io.airlift.http.client.HttpClientConfig;
-import io.airlift.http.client.Request;
-import io.airlift.http.client.jetty.JettyHttpClient;
-import io.airlift.http.client.jetty.JettyIoPool;
-import io.airlift.http.client.jetty.JettyIoPoolConfig;
 import io.airlift.json.JsonCodec;
-import io.airlift.units.Duration;
-
-import javax.annotation.Nullable;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 import java.io.Closeable;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
-import java.net.ProxySelector;
 import java.net.URI;
-import java.util.concurrent.TimeUnit;
 
-import static io.airlift.http.client.HttpUriBuilder.uriBuilderFrom;
-import static io.airlift.http.client.JsonResponseHandler.createJsonResponseHandler;
-import static io.airlift.http.client.Request.Builder.prepareGet;
+import static com.facebook.presto.client.OkHttpUtil.userAgent;
 import static io.airlift.json.JsonCodec.jsonCodec;
 import static java.util.Objects.requireNonNull;
 
 class QueryExecutor
         implements Closeable
 {
-    private final JsonCodec<QueryResults> queryInfoCodec;
-    private final JsonCodec<ServerInfo> serverInfoCodec;
-    private final HttpClient httpClient;
+    private static final JsonCodec<ServerInfo> SERVER_INFO_CODEC = jsonCodec(ServerInfo.class);
 
-    private QueryExecutor(JsonCodec<QueryResults> queryResultsCodec, JsonCodec<ServerInfo> serverInfoCodec, HttpClient httpClient)
+    private final OkHttpClient httpClient;
+
+    private QueryExecutor(OkHttpClient httpClient)
     {
-        this.queryInfoCodec = requireNonNull(queryResultsCodec, "queryResultsCodec is null");
-        this.serverInfoCodec = requireNonNull(serverInfoCodec, "serverInfoCodec is null");
         this.httpClient = requireNonNull(httpClient, "httpClient is null");
     }
 
     public StatementClient startQuery(ClientSession session, String query)
     {
-        return new StatementClient(httpClient, queryInfoCodec, session, query);
+        return new StatementClient(httpClient, session, query);
     }
 
     @Override
     public void close()
     {
-        httpClient.close();
+        httpClient.dispatcher().executorService().shutdown();
+        httpClient.connectionPool().evictAll();
     }
 
     public ServerInfo getServerInfo(URI server)
     {
-        URI uri = uriBuilderFrom(server).replacePath("/v1/info").build();
-        Request request = prepareGet().setUri(uri).build();
-        return httpClient.execute(request, createJsonResponseHandler(serverInfoCodec));
-    }
+        HttpUrl url = HttpUrl.get(server).newBuilder()
+                .encodedPath("/v1/info")
+                .build();
 
-    // TODO: replace this with a phantom reference
-    @SuppressWarnings("FinalizeDeclaration")
-    @Override
-    protected void finalize()
-    {
-        close();
+        Request request = new Request.Builder().url(url).build();
+
+        return JsonResponse.execute(SERVER_INFO_CODEC, httpClient, request).getValue();
     }
 
     static QueryExecutor create(String userAgent)
     {
-        return create(new JettyHttpClient(
-                new HttpClientConfig()
-                        .setConnectTimeout(new Duration(10, TimeUnit.SECONDS))
-                        .setSocksProxy(getSystemSocksProxy()),
-                new JettyIoPool("presto-jdbc", new JettyIoPoolConfig()),
-                ImmutableSet.of(new UserAgentRequestFilter(userAgent))));
-    }
-
-    static QueryExecutor create(HttpClient httpClient)
-    {
-        return new QueryExecutor(jsonCodec(QueryResults.class), jsonCodec(ServerInfo.class), httpClient);
-    }
-
-    @Nullable
-    private static HostAndPort getSystemSocksProxy()
-    {
-        URI uri = URI.create("socket://0.0.0.0:80");
-        for (Proxy proxy : ProxySelector.getDefault().select(uri)) {
-            if (proxy.type() == Proxy.Type.SOCKS) {
-                if (proxy.address() instanceof InetSocketAddress) {
-                    InetSocketAddress address = (InetSocketAddress) proxy.address();
-                    return HostAndPort.fromParts(address.getHostString(), address.getPort());
-                }
-            }
-        }
-        return null;
+        return new QueryExecutor(new OkHttpClient.Builder()
+                .addInterceptor(userAgent(userAgent))
+                .build());
     }
 }