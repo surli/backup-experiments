f0ec23bf563eb7e0836d15c3cc9641a24649c999
Tom <thomas.verin@sonarsource.com>
2017-04-11 11:28:03 +0200

    BUILD: setup ramfs for travis

--- travis.sh
+++ travis.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 set -euo pipefail
 
+# setup ramdisk
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+printf "${RED}disk size before build${NC}\n"
+df -h
+du -sh $HOME
+
+printf "${RED}move original home${NC}\n"
+sudo mv /home/travis /home/travis.ori
+printf "${RED}create ramdisk mount point${NC}\n"
+sudo mkdir -p /home/travis
+printf "${RED}create ramdisk${NC}\n"
+sudo mount -t tmpfs -o size=5120m ext2 /home/travis
+printf "${RED}copy home to ramdisk${NC}\n"
+time sudo cp -R /home/travis.ori/. /home/travis
+printf "${RED}give permissions to travis on its home in ramdisk${NC}\n"
+sudo chown -R travis:travis /home/travis
+printf "${RED}File System:${NC}\n"
+df -h
+# end setup ramdisk
+
 function installPhantomJs {
   echo "Setup PhantomJS 2.1"
   mkdir -p ~/phantomjs
@@ -194,6 +216,12 @@ WEB_TESTS)
   ;;
 
 esac
+
+printf "${RED}disk size after build${NC}\n"
+df -h
+du -sh $HOME
+du -sh $HOME/.m2/repository
+du -sh $HOME/.sonar
 
 #stop the clock
 touch stop
