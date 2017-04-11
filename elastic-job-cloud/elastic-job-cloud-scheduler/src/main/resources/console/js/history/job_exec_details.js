78a3942c14b9b491069a7eb762dfc9ccc3c48468
haocao <caohaoch@gmail.com>
2017-04-11 10:26:02 +0800

    Refactor cloud console history js.

--- elastic-job-cloud/elastic-job-cloud-scheduler/src/main/resources/console/js/history/job_exec_details.js
+++ elastic-job-cloud/elastic-job-cloud-scheduler/src/main/resources/console/js/history/job_exec_details.js
@@ -1,25 +1,36 @@
 $(function() {
+    $("[data-mask]").inputmask();
     $(".toolbar input").bind("keypress", function(event) {
         if("13" === event.keyCode) {
-            $("#job-exec-status-table").bootstrapTable("refresh", {silent: true});
+            $("#job-exec-details-table").bootstrapTable("refresh", {silent: true});
         }
     });
 });
 
 function queryParams(params) {
     return {
-        per_page: params.pageSize, 
+        per_page: params.pageSize,
         page: params.pageNumber,
         q: params.searchText,
         sort: params.sortName,
         order: params.sortOrder,
         jobName: $("#job-name").val(),
         taskId: $("#task-id").val(),
-        slaveId: $("#slave-id").val(),
-        source: $("#source").val(),
-        executionType: $("#execution-type").val(),
-        state: $("#state").val(),
         startTime: $("#start-time").val(),
         endTime: $("#end-time").val(),
+        ip: $("#ip").val(),
+        isSuccess: $('input[name = "isSuccess"]:checked ').val()
     };
+}
+
+function successFormatter(value) {
+    switch(value)
+    {
+    case true:
+      return "Y";
+    case false:
+        return "N";
+    default:
+      return "N/A";
+    }
 }
