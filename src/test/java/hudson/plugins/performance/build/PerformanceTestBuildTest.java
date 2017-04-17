1e0ee2cd96296e0f7f0e41aa0f350537ce12947b
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-04-17 16:20:35 +0300

    full log into file

---
+++ src/test/java/hudson/plugins/performance/build/PerformanceTestBuildTest.java
@@ -0,0 +1,50 @@
+package hudson.plugins.performance.build;
+
+import com.google.common.io.Files;
+import hudson.FilePath;
+import hudson.model.FreeStyleBuild;
+import hudson.model.FreeStyleProject;
+import hudson.model.Result;
+import jenkins.util.BuildListenerAdapter;
+import org.junit.Test;
+import org.jvnet.hudson.test.HudsonTestCase;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+
+
+public class PerformanceTestBuildTest extends HudsonTestCase {
+
+    @Test
+    public void testFlow() throws Exception {
+        String path = getClass().getResource("/performanceTest.yml").getPath();
+
+        PerformanceTestBuild buildTest = new PerformanceTestBuild(path);
+        FreeStyleProject project = createFreeStyleProject();
+        FreeStyleBuildExt buildExt = new FreeStyleBuildExt(project);
+        buildExt.setWorkspace(new FilePath(Files.createTempDir()));
+        buildExt.onStartBuilding();
+
+        buildExt.getRootDir().mkdirs();
+
+        buildTest.perform(buildExt, buildExt.getWorkspace(), createLocalLauncher(), BuildListenerAdapter.wrap(createTaskListener()));
+        assertEquals(Result.SUCCESS, buildExt.getResult());
+    }
+
+    public static class FreeStyleBuildExt extends FreeStyleBuild {
+
+        public FreeStyleBuildExt(FreeStyleProject project) throws IOException {
+            super(project);
+        }
+
+        @Override
+        protected void setWorkspace(@Nonnull FilePath ws) {
+            super.setWorkspace(ws);
+        }
+
+        @Override
+        protected void onStartBuilding() {
+            super.onStartBuilding();
+        }
+    }
+}
