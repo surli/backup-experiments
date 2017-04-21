5d449d827300f57fce58e81ab868d4a02c8caedd
Dmytro Kytsmen <dmitrokytsmen@gmail.com>
2017-04-21 19:15:41 +0300

    Issue #4165: Split and Organize Checkstyle inputs by Test for checks in coding package: NoFinalizer

--- src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/nofinalizer/InputNoFinalizerHasFinalizer.java
+++ src/test/resources/com/puppycrawl/tools/checkstyle/checks/coding/nofinalizer/InputNoFinalizerHasFinalizer.java
@@ -1,13 +1,13 @@
-package com.puppycrawl.tools.checkstyle.checks.coding;
+package com.puppycrawl.tools.checkstyle.checks.coding.nofinalizer;
 
-public class InputHasFinalizer
+public class InputNoFinalizerHasFinalizer
 {
     public void finalize()
     {
         // It's not enough to check if the METHOD_DEF branch contains a PARAMETER_DEF, as that would
         // treat this method as having a parameter.
         Runnable runnable = new Runnable() {
-        
+
             public void run() {
                 reallyFinalize("hi");
             }
@@ -19,7 +19,7 @@ public class InputHasFinalizer
         };
         runnable.run();
     }
-    
+
     // should not be reported by NoFinalizer check
     public void finalize(String x)
     {
