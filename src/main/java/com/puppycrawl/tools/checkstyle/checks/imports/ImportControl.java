de31cca4b1b3a7954f93bf9b18a9ec8b0e99a4a9
Vladislav Lisetskii <vladlis54@gmail.com>
2017-04-22 22:19:14 +0300

    Issue #3496: Enforce ReturnCount max=1 in imports, regexp, naming, design, sizes packages

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/imports/ImportControl.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/imports/ImportControl.java
@@ -271,19 +271,19 @@ class ImportControl {
      * @param inPkg the package doing the import.
      * @return an {@link AccessResult}.
      */
-    private AccessResult localCheckAccess(final String inPkg,
-        final String forImport) {
-        for (AbstractImportRule r : rules) {
+    private AccessResult localCheckAccess(final String inPkg, final String forImport) {
+        AccessResult localCheckAccessResult = AccessResult.UNKNOWN;
+        for (AbstractImportRule importRule : rules) {
             // Check if an import rule is only meant to be applied locally.
-            if (r.isLocalOnly() && !matchesExactly(inPkg)) {
+            if (importRule.isLocalOnly() && !matchesExactly(inPkg)) {
                 continue;
             }
-            final AccessResult result = r.verifyImport(forImport);
+            final AccessResult result = importRule.verifyImport(forImport);
             if (result != AccessResult.UNKNOWN) {
-                return result;
+                localCheckAccessResult = result;
             }
         }
-        return AccessResult.UNKNOWN;
+        return localCheckAccessResult;
     }
 
     /**
