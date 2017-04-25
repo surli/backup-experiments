jcamachor/calcite

[CALCITE-1731] Materialized view rewriting for join and aggregate operators
* Support for rewriting when view contains cardinality-preserving joins that are not present in the query

core/src/main/java/org/apache/calcite/adapter/java/ReflectiveSchema.java
@@ -27,6 +27,7 @@ import org.apache.calcite.linq4j.tree.Expression;
 import org.apache.calcite.linq4j.tree.Expressions;
 import org.apache.calcite.linq4j.tree.Primitive;
 import org.apache.calcite.linq4j.tree.Types;
+import org.apache.calcite.plan.RelOptReferentialConstraint;
 import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
 import org.apache.calcite.schema.Function;
@@ -44,9 +45,12 @@ import org.apache.calcite.schema.impl.AbstractSchema;
 import org.apache.calcite.schema.impl.AbstractTableQueryable;
 import org.apache.calcite.schema.impl.ReflectiveFunctionBase;
 import org.apache.calcite.util.BuiltInMethod;
+import org.apache.calcite.util.Util;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Multimap;
 
 import java.lang.reflect.Constructor;
@@ -54,6 +58,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -90,6 +95,7 @@ public class ReflectiveSchema
     return target;
   }
 
+  @SuppressWarnings({ "rawtypes", "unchecked" })
   @Override protected Map<String, Table> getTableMap() {
     final ImmutableMap.Builder<String, Table> builder = ImmutableMap.builder();
     for (Field field : clazz.getFields()) {
@@ -100,7 +106,28 @@ public class ReflectiveSchema
       }
       builder.put(fieldName, table);
     }
-    return builder.build();
+    Map<String, Table> tableMap = builder.build();
+    // Unique-Key - Foreign-Key
+    for (Field field : clazz.getFields()) {
+      if (RelOptReferentialConstraint.class.isAssignableFrom(field.getType())) {
+        RelOptReferentialConstraint rc;
+        try {
+          rc = (RelOptReferentialConstraint) field.get(target);
+        } catch (IllegalAccessException e) {
+          throw new RuntimeException(
+              "Error while accessing field " + field, e);
+        }
+        FieldTable table =
+            (FieldTable) tableMap.get(Util.last(rc.getForeignKeyTableQualifiedName()));
+        assert table != null;
+        table.statistic = Statistics.of(
+            ImmutableList.copyOf(
+                Iterables.concat(
+                    table.getStatistic().getReferentialConstraints(),
+                    Collections.singleton(rc))));
+      }
+    }
+    return tableMap;
   }
 
   @Override protected Multimap<String, Function> getFunctionMultimap() {
@@ -319,14 +346,25 @@ public class ReflectiveSchema
   /** Table based on a Java field. */
   private static class FieldTable<T> extends ReflectiveTable {
     private final Field field;
+    private Statistic statistic;
 
     FieldTable(Field field, Type elementType, Enumerable<T> enumerable) {
+      this(field, elementType, enumerable, Statistics.UNKNOWN);
+    }
+
+    FieldTable(Field field, Type elementType, Enumerable<T> enumerable,
+        Statistic statistic) {
       super(elementType, enumerable);
       this.field = field;
+      this.statistic = statistic;
     }
 
     public String toString() {
       return "Relation {field=" + field.getName() + "}";
+    }
+
+    @Override public Statistic getStatistic() {
+      return statistic;
     }
 
     @Override public Expression getExpression(SchemaPlus schema,

core/src/main/java/org/apache/calcite/schema/Statistic.java
@@ -16,6 +16,7 @@
  */
 package org.apache.calcite.schema;
 
+import org.apache.calcite.plan.RelOptReferentialConstraint;
 import org.apache.calcite.rel.RelCollation;
 import org.apache.calcite.rel.RelDistribution;
 import org.apache.calcite.util.ImmutableBitSet;
@@ -37,6 +38,10 @@ public interface Statistic {
    * of a unique key, of the table.
    */
   boolean isKey(ImmutableBitSet columns);
+
+  /** Returns the collection of referential constraints (foreign-keys)
+   * for this table. */
+  List<RelOptReferentialConstraint> getReferentialConstraints();
 
   /** Returns the collections of columns on which this table is sorted. */
   List<RelCollation> getCollations();

core/src/main/java/org/apache/calcite/schema/Statistics.java
@@ -16,6 +16,7 @@
  */
 package org.apache.calcite.schema;
 
+import org.apache.calcite.plan.RelOptReferentialConstraint;
 import org.apache.calcite.rel.RelCollation;
 import org.apache.calcite.rel.RelDistribution;
 import org.apache.calcite.rel.RelDistributionTraitDef;
@@ -43,6 +44,10 @@ public class Statistics {
           return false;
         }
 
+        public List<RelOptReferentialConstraint> getReferentialConstraints() {
+          return ImmutableList.<RelOptReferentialConstraint>of();
+        }
+
         public List<RelCollation> getCollations() {
           return ImmutableList.of();
         }
@@ -52,15 +57,33 @@ public class Statistics {
         }
       };
 
-  /** Returns a statistic with a given row count and set of unique keys. */
-  public static Statistic of(final double rowCount,
-      final List<ImmutableBitSet> keys) {
-    return of(rowCount, keys, ImmutableList.<RelCollation>of());
+  /** Returns a statistic with a given set of referential constraints. */
+  public static Statistic of(final List<RelOptReferentialConstraint> referentialConstraints) {
+    return of(null, ImmutableList.<ImmutableBitSet>of(),
+        referentialConstraints, ImmutableList.<RelCollation>of());
   }
 
   /** Returns a statistic with a given row count and set of unique keys. */
   public static Statistic of(final double rowCount,
-      final List<ImmutableBitSet> keys, final List<RelCollation> collations) {
+      final List<ImmutableBitSet> keys) {
+    return of(rowCount, keys, ImmutableList.<RelOptReferentialConstraint>of(),
+        ImmutableList.<RelCollation>of());
+  }
+
+  /** Returns a statistic with a given row count, set of unique keys,
+   * and collations. */
+  public static Statistic of(final double rowCount,
+      final List<ImmutableBitSet> keys,
+      final List<RelCollation> collations) {
+    return of(rowCount, keys, ImmutableList.<RelOptReferentialConstraint>of(), collations);
+  }
+
+  /** Returns a statistic with a given row count, set of unique keys,
+   * referential constraints, and collations. */
+  public static Statistic of(final Double rowCount,
+      final List<ImmutableBitSet> keys,
+      final List<RelOptReferentialConstraint> referentialConstraints,
+      final List<RelCollation> collations) {
     return new Statistic() {
       public Double getRowCount() {
         return rowCount;
@@ -73,6 +96,10 @@ public class Statistics {
           }
         }
         return false;
+      }
+
+      public List<RelOptReferentialConstraint> getReferentialConstraints() {
+        return referentialConstraints;
       }
 
       public List<RelCollation> getCollations() {

core/src/test/java/org/apache/calcite/test/MockCatalogReader.java
@@ -23,6 +23,7 @@ import org.apache.calcite.linq4j.Ord;
 import org.apache.calcite.linq4j.QueryProvider;
 import org.apache.calcite.linq4j.Queryable;
 import org.apache.calcite.linq4j.tree.Expression;
+import org.apache.calcite.plan.RelOptReferentialConstraint;
 import org.apache.calcite.plan.RelOptSchema;
 import org.apache.calcite.plan.RelOptTable;
 import org.apache.calcite.prepare.CalciteCatalogReader;
@@ -669,6 +670,8 @@ public class MockCatalogReader extends CalciteCatalogReader {
     protected final List<Map.Entry<String, RelDataType>> columnList =
         new ArrayList<>();
     protected final List<Integer> keyList = new ArrayList<>();
+    protected final List<RelOptReferentialConstraint> referentialConstraints =
+        new ArrayList<>();
     protected RelDataType rowType;
     protected List<RelCollation> collationList;
     protected final List<String> names;
@@ -857,6 +860,10 @@ public class MockCatalogReader extends CalciteCatalogReader {
     public boolean isKey(ImmutableBitSet columns) {
       return !keyList.isEmpty()
           && columns.contains(ImmutableBitSet.of(keyList));
+    }
+
+    public List<RelOptReferentialConstraint> getReferentialConstraints() {
+      return referentialConstraints;
     }
 
     public RelDataType getRowType() {
@@ -1459,6 +1466,10 @@ public class MockCatalogReader extends CalciteCatalogReader {
 
         public boolean isKey(ImmutableBitSet columns) {
           return table.isKey(columns);
+        }
+
+        public List<RelOptReferentialConstraint> getReferentialConstraints() {
+          return table.getReferentialConstraints();
         }
 
         public List<RelCollation> getCollations() {

surli/backup-experiments

core/src/main/java/org/apache/calcite/adapter/java/ReflectiveSchema.java
@@ -27,6 +27,7 @@ import org.apache.calcite.linq4j.tree.Expression;
 import org.apache.calcite.linq4j.tree.Expressions;
 import org.apache.calcite.linq4j.tree.Primitive;
 import org.apache.calcite.linq4j.tree.Types;
+import org.apache.calcite.rel.RelReferentialConstraint;
 import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
 import org.apache.calcite.schema.Function;
@@ -44,9 +45,12 @@ import org.apache.calcite.schema.impl.AbstractSchema;
 import org.apache.calcite.schema.impl.AbstractTableQueryable;
 import org.apache.calcite.schema.impl.ReflectiveFunctionBase;
 import org.apache.calcite.util.BuiltInMethod;
+import org.apache.calcite.util.Util;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Multimap;
 
 import java.lang.reflect.Constructor;
@@ -54,6 +58,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -90,6 +95,7 @@ public class ReflectiveSchema
     return target;
   }
 
+  @SuppressWarnings({ "rawtypes", "unchecked" })
   @Override protected Map<String, Table> getTableMap() {
     final ImmutableMap.Builder<String, Table> builder = ImmutableMap.builder();
     for (Field field : clazz.getFields()) {
@@ -100,7 +106,28 @@ public class ReflectiveSchema
       }
       builder.put(fieldName, table);
     }
-    return builder.build();
+    Map<String, Table> tableMap = builder.build();
+    // Unique-Key - Foreign-Key
+    for (Field field : clazz.getFields()) {
+      if (RelReferentialConstraint.class.isAssignableFrom(field.getType())) {
+        RelReferentialConstraint rc;
+        try {
+          rc = (RelReferentialConstraint) field.get(target);
+        } catch (IllegalAccessException e) {
+          throw new RuntimeException(
+              "Error while accessing field " + field, e);
+        }
+        FieldTable table =
+            (FieldTable) tableMap.get(Util.last(rc.getSourceQualifiedName()));
+        assert table != null;
+        table.statistic = Statistics.of(
+            ImmutableList.copyOf(
+                Iterables.concat(
+                    table.getStatistic().getReferentialConstraints(),
+                    Collections.singleton(rc))));
+      }
+    }
+    return tableMap;
   }
 
   @Override protected Multimap<String, Function> getFunctionMultimap() {
@@ -319,14 +346,25 @@ public class ReflectiveSchema
   /** Table based on a Java field. */
   private static class FieldTable<T> extends ReflectiveTable {
     private final Field field;
+    private Statistic statistic;
 
     FieldTable(Field field, Type elementType, Enumerable<T> enumerable) {
+      this(field, elementType, enumerable, Statistics.UNKNOWN);
+    }
+
+    FieldTable(Field field, Type elementType, Enumerable<T> enumerable,
+        Statistic statistic) {
       super(elementType, enumerable);
       this.field = field;
+      this.statistic = statistic;
     }
 
     public String toString() {
       return "Relation {field=" + field.getName() + "}";
+    }
+
+    @Override public Statistic getStatistic() {
+      return statistic;
     }
 
     @Override public Expression getExpression(SchemaPlus schema,

core/src/main/java/org/apache/calcite/schema/Statistic.java
@@ -18,6 +18,7 @@ package org.apache.calcite.schema;
 
 import org.apache.calcite.rel.RelCollation;
 import org.apache.calcite.rel.RelDistribution;
+import org.apache.calcite.rel.RelReferentialConstraint;
 import org.apache.calcite.util.ImmutableBitSet;
 
 import java.util.List;
@@ -37,6 +38,10 @@ public interface Statistic {
    * of a unique key, of the table.
    */
   boolean isKey(ImmutableBitSet columns);
+
+  /** Returns the collection of referential constraints (foreign-keys)
+   * for this table. */
+  List<RelReferentialConstraint> getReferentialConstraints();
 
   /** Returns the collections of columns on which this table is sorted. */
   List<RelCollation> getCollations();

core/src/main/java/org/apache/calcite/schema/Statistics.java
@@ -19,6 +19,7 @@ package org.apache.calcite.schema;
 import org.apache.calcite.rel.RelCollation;
 import org.apache.calcite.rel.RelDistribution;
 import org.apache.calcite.rel.RelDistributionTraitDef;
+import org.apache.calcite.rel.RelReferentialConstraint;
 import org.apache.calcite.util.ImmutableBitSet;
 
 import com.google.common.collect.ImmutableList;
@@ -43,6 +44,10 @@ public class Statistics {
           return false;
         }
 
+        public List<RelReferentialConstraint> getReferentialConstraints() {
+          return ImmutableList.<RelReferentialConstraint>of();
+        }
+
         public List<RelCollation> getCollations() {
           return ImmutableList.of();
         }
@@ -52,15 +57,33 @@ public class Statistics {
         }
       };
 
-  /** Returns a statistic with a given row count and set of unique keys. */
-  public static Statistic of(final double rowCount,
-      final List<ImmutableBitSet> keys) {
-    return of(rowCount, keys, ImmutableList.<RelCollation>of());
+  /** Returns a statistic with a given set of referential constraints. */
+  public static Statistic of(final List<RelReferentialConstraint> referentialConstraints) {
+    return of(null, ImmutableList.<ImmutableBitSet>of(),
+        referentialConstraints, ImmutableList.<RelCollation>of());
   }
 
   /** Returns a statistic with a given row count and set of unique keys. */
   public static Statistic of(final double rowCount,
-      final List<ImmutableBitSet> keys, final List<RelCollation> collations) {
+      final List<ImmutableBitSet> keys) {
+    return of(rowCount, keys, ImmutableList.<RelReferentialConstraint>of(),
+        ImmutableList.<RelCollation>of());
+  }
+
+  /** Returns a statistic with a given row count, set of unique keys,
+   * and collations. */
+  public static Statistic of(final double rowCount,
+      final List<ImmutableBitSet> keys,
+      final List<RelCollation> collations) {
+    return of(rowCount, keys, ImmutableList.<RelReferentialConstraint>of(), collations);
+  }
+
+  /** Returns a statistic with a given row count, set of unique keys,
+   * referential constraints, and collations. */
+  public static Statistic of(final Double rowCount,
+      final List<ImmutableBitSet> keys,
+      final List<RelReferentialConstraint> referentialConstraints,
+      final List<RelCollation> collations) {
     return new Statistic() {
       public Double getRowCount() {
         return rowCount;
@@ -73,6 +96,10 @@ public class Statistics {
           }
         }
         return false;
+      }
+
+      public List<RelReferentialConstraint> getReferentialConstraints() {
+        return referentialConstraints;
       }
 
       public List<RelCollation> getCollations() {

core/src/test/java/org/apache/calcite/test/MockCatalogReader.java
@@ -33,6 +33,7 @@ import org.apache.calcite.rel.RelDistribution;
 import org.apache.calcite.rel.RelDistributions;
 import org.apache.calcite.rel.RelFieldCollation;
 import org.apache.calcite.rel.RelNode;
+import org.apache.calcite.rel.RelReferentialConstraint;
 import org.apache.calcite.rel.logical.LogicalFilter;
 import org.apache.calcite.rel.logical.LogicalProject;
 import org.apache.calcite.rel.logical.LogicalTableScan;
@@ -669,6 +670,8 @@ public class MockCatalogReader extends CalciteCatalogReader {
     protected final List<Map.Entry<String, RelDataType>> columnList =
         new ArrayList<>();
     protected final List<Integer> keyList = new ArrayList<>();
+    protected final List<RelReferentialConstraint> referentialConstraints =
+        new ArrayList<>();
     protected RelDataType rowType;
     protected List<RelCollation> collationList;
     protected final List<String> names;
@@ -857,6 +860,10 @@ public class MockCatalogReader extends CalciteCatalogReader {
     public boolean isKey(ImmutableBitSet columns) {
       return !keyList.isEmpty()
           && columns.contains(ImmutableBitSet.of(keyList));
+    }
+
+    public List<RelReferentialConstraint> getReferentialConstraints() {
+      return referentialConstraints;
     }
 
     public RelDataType getRowType() {
@@ -1459,6 +1466,10 @@ public class MockCatalogReader extends CalciteCatalogReader {
 
         public boolean isKey(ImmutableBitSet columns) {
           return table.isKey(columns);
+        }
+
+        public List<RelReferentialConstraint> getReferentialConstraints() {
+          return table.getReferentialConstraints();
         }
 
         public List<RelCollation> getCollations() {