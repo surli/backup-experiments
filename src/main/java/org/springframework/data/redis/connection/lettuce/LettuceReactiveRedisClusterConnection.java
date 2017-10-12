db29d9996583ee4d669573e3da76502dbefe63ce
Luc Esape <luc.esape@gmail.com>
2017-10-13 00:19:06 +0200

    Bug commit from the following repository spring-projects/spring-data-redis This bug commit is a reflect of source code from: http://github.com/spring-projects/spring-data-redis/commit/dcee5e553d18b707a9045f9445357e446b165af3.

---
+++ src/main/java/org/springframework/data/redis/connection/lettuce/LettuceReactiveRedisClusterConnection.java
@@ -0,0 +1,224 @@
+/*
+ * Copyright 2016-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.redis.connection.lettuce;
+
+import io.lettuce.core.api.reactive.BaseRedisReactiveCommands;
+import io.lettuce.core.api.reactive.RedisReactiveCommands;
+import io.lettuce.core.cluster.RedisClusterClient;
+import io.lettuce.core.cluster.api.StatefulRedisClusterConnection;
+import io.lettuce.core.cluster.api.reactive.RedisClusterReactiveCommands;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.nio.ByteBuffer;
+
+import org.springframework.data.redis.connection.ClusterTopologyProvider;
+import org.springframework.data.redis.connection.ReactiveRedisClusterConnection;
+import org.springframework.data.redis.connection.RedisClusterNode;
+import org.springframework.data.redis.connection.RedisNode;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
+
+/**
+ * {@link ReactiveRedisClusterConnection} implementation for {@literal Lettuce}.
+ *
+ * @author Christoph Strobl
+ * @author Mark Paluch
+ * @since 2.0
+ */
+class LettuceReactiveRedisClusterConnection extends LettuceReactiveRedisConnection
+		implements ReactiveRedisClusterConnection {
+
+	private final ClusterTopologyProvider topologyProvider;
+
+	/**
+	 * Creates new {@link LettuceReactiveRedisClusterConnection}.
+	 *
+	 * @param client must not be {@literal null}.
+	 * @throws IllegalArgumentException when {@code client} is {@literal null}.
+	 * @throws org.springframework.dao.InvalidDataAccessResourceUsageException when {@code client} is not suitable for
+	 *           cluster environment.
+	 */
+	LettuceReactiveRedisClusterConnection(LettuceConnectionProvider connectionProvider) {
+
+		super(connectionProvider);
+
+		this.topologyProvider = new LettuceClusterTopologyProvider(
+				((ClusterConnectionProvider) connectionProvider).getClient());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.redis.connection.lettuce.LettuceReactiveRedisConnection#keyCommands()
+	 */
+	@Override
+	public LettuceReactiveClusterKeyCommands keyCommands() {
+		return new LettuceReactiveClusterKeyCommands(this);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.redis.connection.lettuce.LettuceReactiveRedisConnection#listCommands()
+	 */
+	@Override
+	public LettuceReactiveClusterListCommands listCommands() {
+		return new LettuceReactiveClusterListCommands(this);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.redis.connection.lettuce.LettuceReactiveRedisConnection#setCommands()
+	 */
+	@Override
+	public LettuceReactiveClusterSetCommands setCommands() {
+		return new LettuceReactiveClusterSetCommands(this);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.redis.connection.lettuce.LettuceReactiveRedisConnection#zSetCommands()
+	 */
+	@Override
+	public LettuceReactiveClusterZSetCommands zSetCommands() {
+		return new LettuceReactiveClusterZSetCommands(this);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.redis.connection.lettuce.LettuceReactiveRedisConnection#hyperLogLogCommands()
+	 */
+	@Override
+	public LettuceReactiveClusterHyperLogLogCommands hyperLogLogCommands() {
+		return new LettuceReactiveClusterHyperLogLogCommands(this);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.redis.connection.lettuce.LettuceReactiveRedisConnection#stringCommands()
+	 */
+	@Override
+	public LettuceReactiveClusterStringCommands stringCommands() {
+		return new LettuceReactiveClusterStringCommands(this);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.redis.connection.lettuce.LettuceReactiveRedisConnection#geoCommands()
+	 */
+	@Override
+	public LettuceReactiveClusterGeoCommands geoCommands() {
+		return new LettuceReactiveClusterGeoCommands(this);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.redis.connection.lettuce.LettuceReactiveRedisConnection#hashCommands()
+	 */
+	@Override
+	public LettuceReactiveClusterHashCommands hashCommands() {
+		return new LettuceReactiveClusterHashCommands(this);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.redis.connection.lettuce.LettuceReactiveRedisConnection#numberCommands()
+	 */
+	@Override
+	public LettuceReactiveClusterNumberCommands numberCommands() {
+		return new LettuceReactiveClusterNumberCommands(this);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.redis.connection.lettuce.LettuceReactiveRedisConnection#scriptingCommands()
+	 */
+	@Override
+	public LettuceReactiveClusterScriptingCommands scriptingCommands() {
+		return new LettuceReactiveClusterScriptingCommands(this);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.redis.connection.lettuce.LettuceReactiveRedisConnection#serverCommands()
+	 */
+	@Override
+	public LettuceReactiveClusterServerCommands serverCommands() {
+		return new LettuceReactiveClusterServerCommands(this, topologyProvider);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.redis.connection.ReactiveRedisClusterConnection#ping(org.springframework.data.redis.connection.RedisClusterNode)
+	 */
+	@Override
+	public Mono<String> ping(RedisClusterNode node) {
+		return execute(node, BaseRedisReactiveCommands::ping).next();
+	}
+
+	/**
+	 * @param node must not be {@literal null}.
+	 * @param callback must not be {@literal null}.
+	 * @throws IllegalArgumentException when {@code node} or {@code callback} is {@literal null}.
+	 * @return {@link Flux} emitting execution results.
+	 */
+	public <T> Flux<T> execute(RedisNode node, LettuceReactiveCallback<T> callback) {
+
+		try {
+			Assert.notNull(node, "RedisClusterNode must not be null!");
+			Assert.notNull(callback, "ReactiveCallback must not be null!");
+		} catch (IllegalArgumentException e) {
+			return Flux.error(e);
+		}
+
+		return Flux.defer(() -> callback.doWithCommands(getCommands(node))).onErrorMap(translateException());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.redis.connection.lettuce.LettuceReactiveRedisConnection#getConnection()
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	protected StatefulRedisClusterConnection<ByteBuffer, ByteBuffer> getConnection() {
+
+		Assert.isInstanceOf(StatefulRedisClusterConnection.class, super.getConnection(),
+				"Connection needs to be instance of StatefulRedisClusterConnection");
+
+		return (StatefulRedisClusterConnection) super.getConnection();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.redis.connection.lettuce.LettuceReactiveRedisConnection#getCommands()
+	 */
+	protected RedisClusterReactiveCommands<ByteBuffer, ByteBuffer> getCommands() {
+		return getConnection().reactive();
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	protected RedisReactiveCommands<ByteBuffer, ByteBuffer> getCommands(RedisNode node) {
+
+		if (!(getConnection() instanceof StatefulRedisClusterConnection)) {
+			throw new IllegalArgumentException("o.O connection needs to be cluster compatible " + getConnection());
+		}
+
+		if (StringUtils.hasText(node.getId())) {
+			return ((StatefulRedisClusterConnection) getConnection()).getConnection(node.getId()).reactive();
+		}
+
+		return ((StatefulRedisClusterConnection) getConnection()).getConnection(node.getHost(), node.getPort()).reactive();
+	}
+}
---
+++ src/main/java/org/springframework/data/redis/core/RedisKeyExpiredEvent.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.redis.core;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.data.redis.util.ByteUtils;
+import org.springframework.lang.Nullable;
+
+/**
+ * {@link RedisKeyExpiredEvent} is Redis specific {@link ApplicationEvent} published when a specific key in Redis
+ * expires. It might but must not hold the expired value itself next to the key.
+ *
+ * @author Christoph Strobl
+ * @since 1.7
+ */
+public class RedisKeyExpiredEvent<T> extends RedisKeyspaceEvent {
+
+	/**
+	 * Use {@literal UTF-8} as default charset.
+	 */
+	public static final Charset CHARSET = StandardCharsets.UTF_8;
+
+	private final byte[][] args;
+	private final @Nullable Object value;
+
+	/**
+	 * Creates new {@link RedisKeyExpiredEvent}.
+	 *
+	 * @param key
+	 */
+	public RedisKeyExpiredEvent(byte[] key) {
+		this(key, null);
+	}
+
+	/**
+	 * Creates new {@link RedisKeyExpiredEvent}
+	 *
+	 * @param key
+	 * @param value
+	 */
+	public RedisKeyExpiredEvent(byte[] key, @Nullable Object value) {
+		this(null, key, value);
+	}
+
+	/**
+	 * Creates new {@link RedisKeyExpiredEvent}
+	 *
+	 * @pamam channel
+	 * @param key
+	 * @param value
+	 * @since 1.8
+	 */
+	public RedisKeyExpiredEvent(@Nullable String channel, byte[] key, @Nullable Object value) {
+		super(channel, key);
+
+		args = ByteUtils.split(key, ':');
+		this.value = value;
+	}
+
+	/**
+	 * Gets the keyspace in which the expiration occured.
+	 *
+	 * @return {@literal null} if it could not be determined.
+	 */
+	public String getKeyspace() {
+
+		if (args.length >= 2) {
+			return new String(args[0], CHARSET);
+		}
+
+		return null;
+	}
+
+	/**
+	 * Get the expired objects id;
+	 *
+	 * @return
+	 */
+	public byte[] getId() {
+		return args.length == 2 ? args[1] : args[0];
+	}
+
+	/**
+	 * Get the expired Object
+	 *
+	 * @return {@literal null} if not present.
+	 */
+	@Nullable
+	public Object getValue() {
+		return value;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.util.EventObject#toString()
+	 */
+	@Override
+	public String toString() {
+
+		byte[] id = getId();
+		return "RedisKeyExpiredEvent [keyspace=" + getKeyspace() + ", id=" + (id == null ? null : new String(id)) + "]";
+	}
+
+}
