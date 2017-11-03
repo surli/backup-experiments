ff7a618b780be3a5aa462eb65a8eb37404f6f46a
Luc Esape <luc.esape@gmail.com>
2017-11-03 17:46:30 +0100

    Bug commit from the following repository apache/storm This bug commit is a reflect of source code from: http://github.com/apache/storm/commit/ab0556d518257498ca81f68edeaa511d569f2266.

---
+++ storm-server/src/main/java/org/apache/storm/scheduler/resource/ResourceUtils.java
@@ -0,0 +1,316 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.storm.scheduler.resource;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.storm.Config;
+import org.apache.storm.Constants;
+import org.apache.storm.generated.Bolt;
+import org.apache.storm.generated.ComponentCommon;
+import org.apache.storm.generated.SpoutSpec;
+import org.apache.storm.generated.StormTopology;
+import org.apache.storm.utils.ConfigUtils;
+import org.apache.storm.utils.ObjectReader;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.apache.storm.Constants.resourceNameMapping;
+
+public class ResourceUtils {
+    private static final Logger LOG = LoggerFactory.getLogger(ResourceUtils.class);
+
+    public static Map<String, Map<String, Double>> getBoltsResources(StormTopology topology,
+                                                                     Map<String, Object> topologyConf) {
+        Map<String, Map<String, Double>> boltResources = new HashMap<>();
+        if (topology.get_bolts() != null) {
+            for (Map.Entry<String, Bolt> bolt : topology.get_bolts().entrySet()) {
+                Map<String, Double> topologyResources = parseResources(bolt.getValue().get_common().get_json_conf());
+                checkInitialization(topologyResources, bolt.getValue().toString(), topologyConf);
+                LOG.warn("Turned {} into {}", bolt.getValue().get_common().get_json_conf(), topologyResources);
+                boltResources.put(bolt.getKey(), topologyResources);
+            }
+        }
+        return boltResources;
+    }
+
+    public static Map<String, Map<String, Double>> getSpoutsResources(StormTopology topology,
+                                                                      Map<String, Object> topologyConf) {
+        Map<String, Map<String, Double>> spoutResources = new HashMap<>();
+        if (topology.get_spouts() != null) {
+            for (Map.Entry<String, SpoutSpec> spout : topology.get_spouts().entrySet()) {
+                Map<String, Double> topologyResources = parseResources(spout.getValue().get_common().get_json_conf());
+                checkInitialization(topologyResources, spout.getKey(), topologyConf);
+                LOG.warn("Turned {} into {}", spout.getValue().get_common().get_json_conf(), topologyResources);
+                spoutResources.put(spout.getKey(), topologyResources);
+            }
+        }
+        return spoutResources;
+    }
+
+    public static void updateStormTopologyResources(StormTopology topology, Map<String, Map<String, Double>> resourceUpdatesMap) {
+        Map<String, Map<String, Double>> componentsUpdated = new HashMap<>();
+        if (topology.get_spouts() != null) {
+            for (Map.Entry<String, SpoutSpec> spout : topology.get_spouts().entrySet()) {
+                SpoutSpec spoutSpec = spout.getValue();
+                String spoutName = spout.getKey();
+
+                if (resourceUpdatesMap.containsKey(spoutName)) {
+                    ComponentCommon spoutCommon = spoutSpec.get_common();
+                    Map<String, Double> resourcesUpdate = resourceUpdatesMap.get(spoutName);
+                    String newJsonConf = getJsonWithUpdatedResources(spoutCommon.get_json_conf(), resourcesUpdate);
+                    spoutCommon.set_json_conf(newJsonConf);
+                    componentsUpdated.put(spoutName, resourcesUpdate);
+                }
+            }
+        }
+
+        if (topology.get_bolts() != null) {
+            for (Map.Entry<String, Bolt> bolt : topology.get_bolts().entrySet()) {
+                Bolt boltObj = bolt.getValue();
+                String boltName = bolt.getKey();
+
+                if(resourceUpdatesMap.containsKey(boltName)) {
+                    ComponentCommon boltCommon = boltObj.get_common();
+                    Map<String, Double> resourcesUpdate = resourceUpdatesMap.get(boltName);
+                    String newJsonConf = getJsonWithUpdatedResources(boltCommon.get_json_conf(), resourceUpdatesMap.get(boltName));
+                    boltCommon.set_json_conf(newJsonConf);
+                    componentsUpdated.put(boltName, resourcesUpdate);
+                }
+            }
+        }
+        LOG.info("Component resources updated: {}", componentsUpdated);
+        Map<String, Map<String, Double>> notUpdated = new HashMap<String, Map<String, Double>>();
+        for (String component : resourceUpdatesMap.keySet()) {
+            if (!componentsUpdated.containsKey(component)) {
+                notUpdated.put(component, resourceUpdatesMap.get(component));
+            }
+        }
+        LOG.info("Component resource updates ignored: {}", notUpdated);
+    }
+
+    public static String getJsonWithUpdatedResources(String jsonConf, Map<String, Double> resourceUpdates) {
+        try {
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse(jsonConf);
+            JSONObject jsonObject = (JSONObject) obj;
+
+            if (resourceUpdates.containsKey(Config.TOPOLOGY_COMPONENT_RESOURCES_ONHEAP_MEMORY_MB)) {
+                Double topoMemOnHeap = resourceUpdates.get(Config.TOPOLOGY_COMPONENT_RESOURCES_ONHEAP_MEMORY_MB);
+                jsonObject.put(Config.TOPOLOGY_COMPONENT_RESOURCES_ONHEAP_MEMORY_MB, topoMemOnHeap);
+            }
+            if (resourceUpdates.containsKey(Config.TOPOLOGY_COMPONENT_RESOURCES_OFFHEAP_MEMORY_MB)) {
+                Double topoMemOffHeap = resourceUpdates.get(Config.TOPOLOGY_COMPONENT_RESOURCES_OFFHEAP_MEMORY_MB);
+                jsonObject.put(Config.TOPOLOGY_COMPONENT_RESOURCES_OFFHEAP_MEMORY_MB, topoMemOffHeap);
+            }
+            if (resourceUpdates.containsKey(Config.TOPOLOGY_COMPONENT_CPU_PCORE_PERCENT)) {
+                Double topoCPU = resourceUpdates.get(Config.TOPOLOGY_COMPONENT_CPU_PCORE_PERCENT);
+                jsonObject.put(Config.TOPOLOGY_COMPONENT_CPU_PCORE_PERCENT, topoCPU);
+            }
+            return jsonObject.toJSONString();
+        } catch (ParseException ex) {
+            throw new RuntimeException("Failed to parse component resources with json: " +  jsonConf);
+        }
+    }
+
+    public static void checkInitialization(Map<String, Double> topologyResources, String componentId, Map topologyConf) {
+        StringBuilder msgBuilder = new StringBuilder();
+
+        Set<String> resourceNameSet = new HashSet<>();
+
+        resourceNameSet.add(
+                Config.TOPOLOGY_COMPONENT_CPU_PCORE_PERCENT
+        );
+        resourceNameSet.add(
+                Config.TOPOLOGY_COMPONENT_RESOURCES_OFFHEAP_MEMORY_MB
+        );
+        resourceNameSet.add(
+                Config.TOPOLOGY_COMPONENT_RESOURCES_ONHEAP_MEMORY_MB
+        );
+
+
+        resourceNameSet.addAll(topologyResources.keySet());
+        for (String resourceName : resourceNameSet) {
+            msgBuilder.append(checkInitResource(topologyResources, topologyConf, resourceName));
+        }
+
+        Map<String, Double> normalizedTopologyResources = normalizedResourceMap(topologyResources);
+        topologyResources.clear();
+        topologyResources.putAll(normalizedTopologyResources);
+
+        if (msgBuilder.length() > 0) {
+            String resourceDefaults = msgBuilder.toString();
+            LOG.debug(
+                    "Unable to extract resource requirement for Component {} \n Resources : {}",
+                    componentId, resourceDefaults);
+        }
+    }
+
+    private static String checkInitResource(Map<String, Double> topologyResources, Map topologyConf, String resourceName) {
+        StringBuilder msgBuilder = new StringBuilder();
+        if (!topologyResources.containsKey(resourceNameMapping.get(resourceName))) {
+            if (topologyConf.containsKey(resourceName)) {
+                Double resourceValue = ObjectReader.getDouble(topologyConf.get(resourceName));
+                topologyResources.put(resourceNameMapping.get(resourceName), resourceValue);
+            }
+        }
+        return msgBuilder.toString();
+    }
+
+    public static Map<String, Double> parseResources(String input) {
+        Map<String, Double> topologyResources = new HashMap<>();
+        JSONParser parser = new JSONParser();
+        LOG.debug("Input to parseResources {}", input);
+        try {
+            if (input != null) {
+                Object obj = parser.parse(input);
+                JSONObject jsonObject = (JSONObject) obj;
+
+                // Legacy resource parsing
+                if (jsonObject.containsKey(Config.TOPOLOGY_COMPONENT_RESOURCES_ONHEAP_MEMORY_MB)) {
+                    Double topoMemOnHeap = ObjectReader
+                            .getDouble(jsonObject.get(Config.TOPOLOGY_COMPONENT_RESOURCES_ONHEAP_MEMORY_MB), null);
+                    topologyResources.put(Config.TOPOLOGY_COMPONENT_RESOURCES_ONHEAP_MEMORY_MB, topoMemOnHeap);
+                }
+                if (jsonObject.containsKey(Config.TOPOLOGY_COMPONENT_RESOURCES_OFFHEAP_MEMORY_MB)) {
+                    Double topoMemOffHeap = ObjectReader
+                            .getDouble(jsonObject.get(Config.TOPOLOGY_COMPONENT_RESOURCES_OFFHEAP_MEMORY_MB), null);
+                    topologyResources.put(Config.TOPOLOGY_COMPONENT_RESOURCES_OFFHEAP_MEMORY_MB, topoMemOffHeap);
+                }
+                if (jsonObject.containsKey(Config.TOPOLOGY_COMPONENT_CPU_PCORE_PERCENT)) {
+                    Double topoCpu = ObjectReader.getDouble(jsonObject.get(Config.TOPOLOGY_COMPONENT_CPU_PCORE_PERCENT),
+                        null);
+                    topologyResources.put(Config.TOPOLOGY_COMPONENT_CPU_PCORE_PERCENT, topoCpu);
+                }
+
+                // If resource is also present in resources map will overwrite the above
+                if (jsonObject.containsKey(Config.TOPOLOGY_COMPONENT_RESOURCES_MAP)) {
+                    topologyResources.putAll((Map<String, Double>) jsonObject.get(Config.TOPOLOGY_COMPONENT_RESOURCES_MAP));
+                }
+                    LOG.info("Topology Resources {}", topologyResources);
+            }
+        } catch (ParseException e) {
+            LOG.error("Failed to parse component resources is:" + e.toString(), e);
+            return null;
+        }
+        return normalizedResourceMap(topologyResources);
+    }
+
+    private static void debugMessage(String memoryType, String com, Map<String, Object> topologyConf) {
+        if (memoryType.equals("ONHEAP")) {
+            LOG.debug(
+                    "Unable to extract resource requirement for Component {}\n"
+                        + " Resource : Memory Type : On Heap set to default {}",
+                    com, topologyConf.get(Config.TOPOLOGY_COMPONENT_RESOURCES_ONHEAP_MEMORY_MB));
+        } else if (memoryType.equals("OFFHEAP")) {
+            LOG.debug(
+                    "Unable to extract resource requirement for Component {}\n"
+                        + " Resource : Memory Type : Off Heap set to default {}",
+                    com, topologyConf.get(Config.TOPOLOGY_COMPONENT_RESOURCES_OFFHEAP_MEMORY_MB));
+        } else {
+            LOG.debug(
+                    "Unable to extract resource requirement for Component {}\n"
+                        + " Resource : CPU Pcore Percent set to default {}",
+                    com, topologyConf.get(Config.TOPOLOGY_COMPONENT_CPU_PCORE_PERCENT));
+        }
+    }
+
+    /**
+     * Calculate the sum of a collection of doubles.
+     * @param list collection of doubles
+     * @return the sum of of collection of doubles
+     */
+    public static double sum(Collection<Double> list) {
+        double sum = 0.0;
+        for (Double elem : list) {
+            sum += elem;
+        }
+        return sum;
+    }
+
+    /**
+     * Calculate the average of a collection of doubles.
+     * @param list a collection of doubles
+     * @return the average of collection of doubles
+     */
+    public static double avg(Collection<Double> list) {
+        return sum(list) / list.size();
+    }
+
+    /**
+     * Normalizes a supervisor resource map or topology details map's keys to universal resource names.
+     * @param resourceMap resource map of either Supervisor or Topology
+     * @return the resource map with common resource names
+     */
+    public static Map<String, Double> normalizedResourceMap(Map<String, Double> resourceMap) {
+        Map<String, Double> result = new HashMap();
+
+        result.putAll(resourceMap);
+        for(Map.Entry entry: resourceMap.entrySet()) {
+            if (resourceNameMapping.containsKey(entry.getKey())) {
+                result.put(resourceNameMapping.get(entry.getKey()), ObjectReader.getDouble(entry.getValue(), 0.0));
+                result.remove(entry.getKey());
+            }
+        }
+        return result;
+    }
+
+    public static Map<String, Double> addResources(Map<String, Double> resourceMap1, Map<String, Double> resourceMap2) {
+        Map<String, Double> result = new HashMap();
+
+        result.putAll(resourceMap1);
+
+        for(Map.Entry<String, Double> entry: resourceMap2.entrySet()) {
+            if (result.containsKey(entry.getKey())) {
+                result.put(entry.getKey(), ObjectReader.getDouble(entry.getValue(),
+                        0.0) + ObjectReader.getDouble(resourceMap1.get(entry.getKey()), 0.0));
+            } else {
+                result.put(entry.getKey(), entry.getValue());
+            }
+        }
+        return result;
+
+    }
+
+    public static Double getMinValuePresentInResourceMap(Map<String, Double> resourceMap) {
+        return Collections.min(resourceMap.values());
+    }
+
+    public static Map<String, Double> getPercentageOfTotalResourceMap(Map<String, Double> resourceMap, Map<String, Double> totalResourceMap) {
+        Map<String, Double> result = new HashMap();
+
+        for(Map.Entry<String, Double> entry: totalResourceMap.entrySet()) {
+            if (resourceMap.containsKey(entry.getKey())) {
+                result.put(entry.getKey(),  (ObjectReader.getDouble(resourceMap.get(entry.getKey()))/ entry.getValue()) * 100.0) ;
+            } else {
+                result.put(entry.getKey(), 0.0);
+            }
+        }
+        return result;
+
+    }
+}
---
+++ storm-server/src/main/java/org/apache/storm/scheduler/resource/strategies/scheduling/GenericResourceAwareStrategy.java
@@ -0,0 +1,188 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.storm.scheduler.resource.strategies.scheduling;
+
+import com.google.common.annotations.VisibleForTesting;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
+
+import org.apache.storm.Config;
+import org.apache.storm.scheduler.Cluster;
+import org.apache.storm.scheduler.Component;
+import org.apache.storm.scheduler.ExecutorDetails;
+import org.apache.storm.scheduler.TopologyDetails;
+import org.apache.storm.scheduler.resource.ResourceUtils;
+import org.apache.storm.scheduler.resource.SchedulingResult;
+import org.apache.storm.scheduler.resource.SchedulingStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GenericResourceAwareStrategy extends BaseResourceAwareStrategy implements IStrategy {
+    private static final Logger LOG = LoggerFactory.getLogger(GenericResourceAwareStrategy.class);
+
+    @Override
+    public SchedulingResult schedule(Cluster cluster, TopologyDetails td) {
+        prepare(cluster);
+        if (nodes.getNodes().size() <= 0) {
+            LOG.warn("No available nodes to schedule tasks on!");
+            return SchedulingResult.failure(
+                    SchedulingStatus.FAIL_NOT_ENOUGH_RESOURCES, "No available nodes to schedule tasks on!");
+        }
+        Collection<ExecutorDetails> unassignedExecutors =
+                new HashSet<>(this.cluster.getUnassignedExecutors(td));
+        LOG.info("ExecutorsNeedScheduling: {}", unassignedExecutors);
+        Collection<ExecutorDetails> scheduledTasks = new ArrayList<>();
+        List<Component> spouts = this.getSpouts(td);
+
+        if (spouts.size() == 0) {
+            LOG.error("Cannot find a Spout!");
+            return SchedulingResult.failure(
+                    SchedulingStatus.FAIL_INVALID_TOPOLOGY, "Cannot find a Spout!");
+        }
+
+        //order executors to be scheduled
+        List<ExecutorDetails> orderedExecutors = this.orderExecutors(td, unassignedExecutors);
+        Collection<ExecutorDetails> executorsNotScheduled = new HashSet<>(unassignedExecutors);
+        List<String> favoredNodes = (List<String>) td.getConf().get(Config.TOPOLOGY_SCHEDULER_FAVORED_NODES);
+        List<String> unFavoredNodes = (List<String>) td.getConf().get(Config.TOPOLOGY_SCHEDULER_UNFAVORED_NODES);
+
+        for (ExecutorDetails exec : orderedExecutors) {
+            LOG.debug(
+                    "Attempting to schedule: {} of component {}[ REQ {} ]",
+                    exec,
+                    td.getExecutorToComponent().get(exec),
+                    td.getTaskResourceReqList(exec));
+            final List<ObjectResources> sortedNodes = this.sortAllNodes(td, exec, favoredNodes, unFavoredNodes);
+
+            scheduleExecutor(exec, td, scheduledTasks, sortedNodes);
+        }
+
+        executorsNotScheduled.removeAll(scheduledTasks);
+        LOG.error("/* Scheduling left over task (most likely sys tasks) */");
+        // schedule left over system tasks
+        for (ExecutorDetails exec : executorsNotScheduled) {
+            final List<ObjectResources> sortedNodes = this.sortAllNodes(td, exec, favoredNodes, unFavoredNodes);
+            scheduleExecutor(exec, td, scheduledTasks, sortedNodes);
+        }
+
+        SchedulingResult result;
+        executorsNotScheduled.removeAll(scheduledTasks);
+        if (executorsNotScheduled.size() > 0) {
+            LOG.error("Not all executors successfully scheduled: {}", executorsNotScheduled);
+            result =
+                    SchedulingResult.failure(
+                            SchedulingStatus.FAIL_NOT_ENOUGH_RESOURCES,
+                            (td.getExecutors().size() - unassignedExecutors.size())
+                                    + "/"
+                                    + td.getExecutors().size()
+                                    + " executors scheduled");
+        } else {
+            LOG.debug("All resources successfully scheduled!");
+            result = SchedulingResult.success("Fully Scheduled by " + this.getClass().getSimpleName());
+        }
+        return result;
+    }
+
+
+    /**
+     * Sort objects by the following two criteria. 1) the number executors of the topology that needs
+     * to be scheduled is already on the object (node or rack) in descending order. The reasoning to
+     * sort based on criterion 1 is so we schedule the rest of a topology on the same object (node or
+     * rack) as the existing executors of the topology. 2) the subordinate/subservient resource
+     * availability percentage of a rack in descending order We calculate the resource availability
+     * percentage by dividing the resource availability of the object (node or rack) by the resource
+     * availability of the entire rack or cluster depending on if object references a node or a rack.
+     * By doing this calculation, objects (node or rack) that have exhausted or little of one of the
+     * resources mentioned above will be ranked after racks that have more balanced resource
+     * availability. So we will be less likely to pick a rack that have a lot of one resource but a
+     * low amount of another.
+     *
+     * @param allResources contains all individual ObjectResources as well as cumulative stats
+     * @param existingScheduleFunc a function to get existing executors already scheduled on this object
+     * @return a sorted list of ObjectResources
+     */
+    @Override
+    protected TreeSet<ObjectResources> sortObjectResources(
+            final AllResources allResources, ExecutorDetails exec, TopologyDetails topologyDetails,
+            final ExistingScheduleFunc existingScheduleFunc) {
+
+        Map<String, Double> requestedResources = topologyDetails.getTotalResources(exec);
+        AllResources affinityBasedAllResources = new AllResources(allResources);
+        for (ObjectResources objectResources : affinityBasedAllResources.objectResources) {
+            StringBuilder sb = new StringBuilder();
+            List<Double> values = new LinkedList<>();
+
+            for(Map.Entry<String, Double> availableResourcesEntry : objectResources.availableResources.entrySet()) {
+                if(!requestedResources.containsKey(availableResourcesEntry.getKey())) {
+                    objectResources.availableResources.put(availableResourcesEntry.getKey(), -1.0 * availableResourcesEntry.getValue());
+                }
+            }
+
+            Map<String, Double> percentageTotal = ResourceUtils.getPercentageOfTotalResourceMap(
+                    objectResources.availableResources, allResources.availableResourcesOverall
+            );
+            for(Map.Entry<String, Double> percentageEntry : percentageTotal.entrySet()) {
+                values.add(percentageEntry.getValue());
+                sb.append(String.format("%s %f(%f%%) ", percentageEntry.getKey(),
+                        objectResources.availableResources.get(percentageEntry.getKey()),
+                        percentageEntry.getValue())
+                );
+
+            }
+
+        }
+
+        TreeSet<ObjectResources> sortedObjectResources =
+                new TreeSet<>((o1, o2) -> {
+                    int execsScheduled1 = existingScheduleFunc.getNumExistingSchedule(o1.id);
+                    int execsScheduled2 = existingScheduleFunc.getNumExistingSchedule(o2.id);
+                    if (execsScheduled1 > execsScheduled2) {
+                        return -1;
+                    } else if (execsScheduled1 < execsScheduled2) {
+                        return 1;
+                    } else {
+                        Collection<Double> o1Values = ResourceUtils.getPercentageOfTotalResourceMap(
+                                o1.availableResources, allResources.availableResourcesOverall).values();
+                        Collection<Double> o2Values = ResourceUtils.getPercentageOfTotalResourceMap(
+                                o2.availableResources, allResources.availableResourcesOverall).values();
+
+                        double o1Avg = ResourceUtils.avg(o1Values);
+                        double o2Avg = ResourceUtils.avg(o2Values);
+
+                        if (o1Avg > o2Avg) {
+                            return -1;
+                        } else if (o1Avg < o2Avg) {
+                            return 1;
+                        } else {
+                            return o1.id.compareTo(o2.id);
+                        }
+
+                    }
+                });
+        sortedObjectResources.addAll(affinityBasedAllResources.objectResources);
+        LOG.debug("Sorted Object Resources: {}", sortedObjectResources);
+        return sortedObjectResources;
+    }
+}
