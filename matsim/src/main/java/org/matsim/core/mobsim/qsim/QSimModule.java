00371b0bbe66671228a02e03b4b4bffbc5e93782
Luc Esape <luc.esape@gmail.com>
2017-11-05 23:32:59 +0100

    Bug commit from the following repository matsim-org/matsim This bug commit is a reflect of source code from: http://github.com/matsim-org/matsim/commit/f3ba0f41da364c169c732f3a4d7ea47f020e5062.

---
+++ matsim/src/main/java/org/matsim/core/mobsim/qsim/QSimModule.java
@@ -0,0 +1,57 @@
+package org.matsim.core.mobsim.qsim;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.inject.Inject;
+
+import org.matsim.core.config.Config;
+import org.matsim.core.mobsim.framework.Mobsim;
+import org.matsim.core.mobsim.qsim.changeeventsengine.NetworkChangeEventsPlugin;
+import org.matsim.core.mobsim.qsim.messagequeueengine.MessageQueuePlugin;
+import org.matsim.core.mobsim.qsim.pt.ComplexTransitStopHandlerFactory;
+import org.matsim.core.mobsim.qsim.pt.TransitEnginePlugin;
+import org.matsim.core.mobsim.qsim.pt.TransitStopHandlerFactory;
+import org.matsim.core.mobsim.qsim.qnetsimengine.DefaultQNetworkFactory;
+import org.matsim.core.mobsim.qsim.qnetsimengine.QLanesNetworkFactory;
+import org.matsim.core.mobsim.qsim.qnetsimengine.QNetsimEnginePlugin;
+import org.matsim.core.mobsim.qsim.qnetsimengine.QNetworkFactory;
+
+import com.google.inject.Provides;
+
+public class QSimModule extends com.google.inject.AbstractModule {
+	@Inject Config config ;
+	
+	@Override
+	protected void configure() {
+		bind(Mobsim.class).toProvider(QSimProvider.class);
+		if ( config.qsim().isUseLanes() ) { 
+			bind(QNetworkFactory.class).to( QLanesNetworkFactory.class ) ;
+		} else {
+			bind(QNetworkFactory.class).to( DefaultQNetworkFactory.class ) ;
+		}
+		if ( config.transit().isUseTransit() ) {
+			bind( TransitStopHandlerFactory.class ).to( ComplexTransitStopHandlerFactory.class ) ;
+		}
+		// yy see MATSIM-756
+	}
+
+	@SuppressWarnings("static-method")
+	@Provides
+	Collection<AbstractQSimPlugin> provideQSimPlugins(Config config1) {
+		final Collection<AbstractQSimPlugin> plugins = new ArrayList<>();
+		plugins.add(new MessageQueuePlugin(config1));
+		plugins.add(new ActivityEnginePlugin(config1));
+		plugins.add(new QNetsimEnginePlugin(config1));
+		if (config1.network().isTimeVariantNetwork()) {
+			plugins.add(new NetworkChangeEventsPlugin(config1));
+		}
+		if (config1.transit().isUseTransit()) {
+			plugins.add(new TransitEnginePlugin(config1));
+		}
+		plugins.add(new TeleportationPlugin(config1));
+		plugins.add(new PopulationPlugin(config1));
+		return plugins;
+	}
+}
