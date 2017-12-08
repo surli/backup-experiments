4a8c723c57e60035f73cf671b77c241f75973725
Luc Esape <luc.esape@gmail.com>
2017-12-08 16:09:20 +0100

    Bug commit from the following repository phoenixnap/springmvc-raml-plugin This bug commit is a reflect of source code from: http://github.com/phoenixnap/springmvc-raml-plugin/commit/7f51e1c26c99e515d02f7c6678e10dafc1595a06.

---
+++ springmvc-raml-parser/src/test/java/com/phoenixnap/oss/ramlapisync/generation/rules/ShortcutMappingsTest.java
@@ -0,0 +1,37 @@
+package com.phoenixnap.oss.ramlapisync.generation.rules;
+
+import com.phoenixnap.oss.ramlapisync.generation.RamlParser;
+import com.phoenixnap.oss.ramlapisync.pojo.PojoGenerationConfig;
+import com.phoenixnap.oss.ramlapisync.raml.InvalidRamlResourceException;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author aleksandars
+ * @since 0.10.5
+ */
+public class ShortcutMappingsTest extends AbstractRuleTestBase {
+
+	private Spring4ControllerInterfaceRule rule;
+
+	public ShortcutMappingsTest() {
+		super();
+		defaultRamlParser = new RamlParser(new PojoGenerationConfig().withPackage("com.gen.test", null), "/api", false,
+				false, 2, 0, false);
+	}
+
+	@BeforeClass
+	public static void initRaml() throws InvalidRamlResourceException {
+		AbstractRuleTestBase.RAML = RamlLoader.loadRamlFromFile(AbstractRuleTestBase.RESOURCE_BASE + "shortcut-mappings.raml");
+	}
+
+	@Test
+	public void applySpring4ControllerInterfaceWithShortcutAnnotationsRule_shouldCreate_validCode() throws Exception {
+
+		rule = new Spring4ControllerInterfaceRule();
+		rule.setUseShortcutMethodMappings(true);
+		rule.apply(getControllerMetadata(), jCodeModel);
+		String removedSerialVersionUID = removeSerialVersionUID(serializeModel());
+		verifyGeneratedCode("Spring4ControllerInterfaceWithShortcutMappings", removedSerialVersionUID);
+	}
+}
